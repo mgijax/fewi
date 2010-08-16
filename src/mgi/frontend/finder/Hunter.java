c10130db35adb133cb78a58eda8b99ce236228b8
Peter Frost <Pete.Frost@jax.org>
2010-08-16 18:36:37 +0000

    Reorganizing package structure

--- src/mgi/frontend/finder/Hunter.java
+++
@@ -1,9 +0,0 @@
-package mgi.frontend.finder;
-
-import java.util.List;
-
-public interface Hunter<T> {
-	
-	List<T> search();
-
-}
--- src/mgi/frontend/finder/ReferenceGatherer.java
+++
@@ -1,7 +0,0 @@
-package mgi.frontend.finder;
-
-import mgi.frontend.datamodel.Reference;
-
-public interface ReferenceGatherer extends GenericGatherer<Reference> {
-
-}
