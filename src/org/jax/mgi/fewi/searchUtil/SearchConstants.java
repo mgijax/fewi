5baa4790f537bac9d6085acef11f596ece08374d
Matt Hall <Matt.Hall@jax.org>
2010-08-25 15:13:46 +0000

    Changed the naming convention of these constants.

--- src/org/jax/mgi/fewi/searchUtil/SearchConstants.java
+++ src/org/jax/mgi/fewi/searchUtil/SearchConstants.java
@@ -9,8 +9,8 @@ package org.jax.mgi.fewi.searchUtil;
 public class SearchConstants {
 
 	// reference constants
-    public static final String REFERENCE_ID			= "reference_id";
-    public static final String REFERENCE_KEY        = "reference_key";
+    public static final String REF_ID			    = "reference_id";
+    public static final String REF_KEY              = "reference_key";
     public static final String REF_AUTHOR_ANY		= "author_any";
     public static final String REF_AUTHOR_FIRST		= "author_first";
     public static final String REF_AUTHOR_LAST		= "author_last";
@@ -19,10 +19,10 @@ public class SearchConstants {
     public static final String REF_TEXT_TITLE       = "text_title";
     
     // sequence constants
-    public static final String SEQUENCE_ID          = "sequence_id";
-    public static final String SEQUENCE_KEY         = "sequence_key";
+    public static final String SEQ_ID               = "sequence_id";
+    public static final String SEQ_KEY              = "sequence_key";
     
     // marker constants
-    public static final String MARKER_KEY           = "marker_key";
+    public static final String MRK_KEY              = "marker_key";
     
 }
