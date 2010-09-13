a8d28120311ac9758bca9eb8b16b0e609fbb5e85
Mark Airey <Mark.Airey@jax.org>
2010-09-13 11:14:17 +0000

    adding WebTemplate to loader

--- src/org/jax/mgi/fewi/config/ConfigLoader.java
+++ src/org/jax/mgi/fewi/config/ConfigLoader.java
@@ -3,16 +3,24 @@ package org.jax.mgi.fewi.config;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 
+import org.jax.mgi.fewi.template.WebTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.ServletContextAware;
 
 @Component
 public class ConfigLoader implements ServletContextAware {
+	
+	private Logger logger = LoggerFactory.getLogger(ConfigLoader.class);
     private ServletContext servletContext;
     
     @Autowired
     private ConfigWrapper configWrapper;
+    
+    @Autowired
+    private WebTemplate webTemplate;
 
 	public void setServletContext(ServletContext servletContext) {
 		this.servletContext = servletContext;
@@ -21,5 +29,7 @@ public class ConfigLoader implements ServletContextAware {
 	@PostConstruct
     public void init() {
             servletContext.setAttribute("configBean", configWrapper);
+            servletContext.setAttribute("templateBean", webTemplate);
+            logger.debug("configs loaded");
     }
 }
