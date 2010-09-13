6bfca0f0e2845aefaec3b52d3ab7230d5f729222
Mark Airey <Mark.Airey@jax.org>
2010-09-13 13:16:53 +0000

    sort, pagination & history manager working

--- src/org/jax/mgi/fewi/controller/ReferenceController.java
+++ src/org/jax/mgi/fewi/controller/ReferenceController.java
@@ -11,9 +11,11 @@ import mgi.frontend.datamodel.Reference;
 import org.jax.mgi.fewi.finder.ReferenceFinder;
 import org.jax.mgi.fewi.forms.ReferenceQueryForm;
 import org.jax.mgi.fewi.searchUtil.Filter;
+import org.jax.mgi.fewi.searchUtil.MySortPaginator;
 import org.jax.mgi.fewi.searchUtil.SearchConstants;
 import org.jax.mgi.fewi.searchUtil.SearchParams;
 import org.jax.mgi.fewi.searchUtil.SearchResults;
+import org.jax.mgi.fewi.searchUtil.Sort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +38,7 @@ public class ReferenceController {
 	@RequestMapping(method=RequestMethod.GET)
 	public String getQueryForm(Model model) {
 		model.addAttribute(new ReferenceQueryForm());
+		model.addAttribute("sort", new MySortPaginator());
 		return "reference_query";
 	}
 	
@@ -43,33 +46,60 @@ public class ReferenceController {
 	public String referenceSummary(HttpServletRequest request, Model model) {
 
 		model.addAttribute("queryString", request.getQueryString());
-		logger.info("queryString: " + request.getQueryString());
+		logger.debug("queryString: " + request.getQueryString());
 
 		return "reference_summary";
 	}
 	
 	@RequestMapping("/json")
 	public @ResponseBody SearchResults<Reference> referenceSummaryJson(
-			@ModelAttribute ReferenceQueryForm query) {
+			@ModelAttribute ReferenceQueryForm query,
+			@ModelAttribute MySortPaginator mySort) {
+				
+		logger.debug("mysort: " + mySort.toString());
 		
-		SearchParams params = new SearchParams();		
+		SearchParams params = new SearchParams();
+		List<Sort> sorts = new ArrayList<Sort>();
+		
+		String sortStr = mySort.getSort();
+		if (sortStr == null || "".equals(sortStr)){
+			mySort.setSort("journal");
+		}
+		
+		Sort sort = new Sort(mySort.getSort());
+		if (mySort.getDir().equals("asc")){
+			sort.setDesc(true);
+		} else {
+			sort.setDesc(false);
+		}
+		sorts.add(sort);
+		params.setSorts(sorts);
+		
+		params.setStartIndex(mySort.getStartIndex());
+		params.setPageSize(mySort.getResults());
+		
 		List<Filter> filterList = new ArrayList<Filter>();
 		
 		if(query.getAuthor() != null && !"".equals(query.getAuthor())){
 			List<String> authors = Arrays.asList(query.getAuthor().split(";"));
+			logger.debug("authors: ", authors.toArray());
 			String scope = query.getAuthorScope();
 			if ("first".equals(scope)){
-				filterList.add(new Filter(SearchConstants.REF_AUTHOR_FIRST, authors, Filter.OP_EQUAL));
+				filterList.add(new Filter(SearchConstants.REF_AUTHOR_FIRST, authors, Filter.OP_IN));
 			} else if ("last".equals(scope)){
-				filterList.add(new Filter(SearchConstants.REF_AUTHOR_LAST, authors, Filter.OP_EQUAL));
+				filterList.add(new Filter(SearchConstants.REF_AUTHOR_LAST, authors, Filter.OP_IN));
 			} else {
-				filterList.add(new Filter(SearchConstants.REF_AUTHOR_ANY, authors, Filter.OP_EQUAL));
+				filterList.add(new Filter(SearchConstants.REF_AUTHOR_ANY, authors, Filter.OP_IN));
 			}
 		}
 
 		if(query.getJournal() != null && !"".equals(query.getJournal())){
 			List<String> journals = Arrays.asList(query.getJournal().split(";"));
 			filterList.add(new Filter(SearchConstants.REF_JOURNAL, journals, Filter.OP_EQUAL));
+		}
+		
+		if(query.getYear() != null ){
+			filterList.add(new Filter(SearchConstants.REF_YEAR, query.getYear().toString(), Filter.OP_EQUAL));
 		}
 		
 		if(query.getText() != null && !"".equals(query.getText())){
@@ -87,12 +117,14 @@ public class ReferenceController {
 				filterList.add(textFilters.get(0));
 			} else {
 				f.setFilterJoinClause(Filter.FC_OR);
+				f.setNestedFilters(textFilters);
 				filterList.add(f);
 			}
 		}
 		
 		if (filterList.size() > 0){
 			Filter f = new Filter();
+			f.setFilterJoinClause(Filter.FC_AND);
 			f.setNestedFilters(filterList);
 			params.setFilter(f);
 		} else if (query.getId() != null && !"".equals(query.getId())){
