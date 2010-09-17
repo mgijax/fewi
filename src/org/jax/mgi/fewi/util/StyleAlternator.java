ad50c96c18c6aaf7d2c272d6386e241fac5bcaef
Peter Frost <Pete.Frost@jax.org>
2010-09-17 17:07:54 +0000

    Initial version

---
+++ src/org/jax/mgi/fewi/util/StyleAlternator.java
@@ -0,0 +1,56 @@
+package org.jax.mgi.fewi.util;
+
+import java.util.*;
+
+/**
+ * provides an easy mechanism to alternate styles
+ */
+public class StyleAlternator
+{
+  //--------------//
+  // class fields
+  //--------------//
+
+  // internal list of the two strings to cycle
+  private String[] styles = new String[2];
+
+  // index into 'styles' array;  used to determine which style to return
+  private int styleIndex = 0;
+
+
+  //---------------------//
+  // public constructors
+  //---------------------//
+
+  /** instantiates a new 'StyleAlternator' to cycle through
+   * @param str1
+   * @param str2
+   */
+  public StyleAlternator(String str1, String str2)
+  {
+
+      styles[0] = str1;
+      styles[1] = str2;
+  }
+
+  //------------------------//
+  // public instance methods
+  //------------------------//
+
+  /**
+   * get the next string
+   * @return String
+   */
+  public String getNext()
+  {
+      String style = styles[styleIndex];
+
+      styleIndex++;
+      if (styleIndex == styles.length) {
+          styleIndex = 0;
+      }
+
+      return style;
+  }
+
+}
