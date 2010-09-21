d6032dd3706c0dba9cb8d9302ee87e38e7903fa6
Mark Airey <Mark.Airey@jax.org>
2010-09-21 02:09:31 +0000

    refactor parameter parsing

--- src/org/jax/mgi/fewi/controller/ReferenceController.java
+++ src/org/jax/mgi/fewi/controller/ReferenceController.java
@@ -11,11 +11,12 @@ import mgi.frontend.datamodel.Reference;
 import org.jax.mgi.fewi.finder.ReferenceFinder;
 import org.jax.mgi.fewi.forms.ReferenceQueryForm;
 import org.jax.mgi.fewi.searchUtil.Filter;
-import org.jax.mgi.fewi.searchUtil.MySortPaginator;
+import org.jax.mgi.fewi.searchUtil.Paginator;
 import org.jax.mgi.fewi.searchUtil.SearchConstants;
 import org.jax.mgi.fewi.searchUtil.SearchParams;
 import org.jax.mgi.fewi.searchUtil.SearchResults;
 import org.jax.mgi.fewi.searchUtil.Sort;
+import org.jax.mgi.fewi.searchUtil.SortConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,7 +46,7 @@ public class ReferenceController {
 	@RequestMapping(method=RequestMethod.GET)
 	public String getQueryForm(Model model) {
 		model.addAttribute(new ReferenceQueryForm());
-		model.addAttribute("sort", new MySortPaginator());
+		model.addAttribute("sort", new Paginator());
 		return "reference_query";
 	}
 	
@@ -65,35 +66,23 @@ public class ReferenceController {
 	// this is the logis to perform the query and retur json results
 	@RequestMapping("/json")
 	public @ResponseBody SearchResults<Reference> referenceSummaryJson(
+			HttpServletRequest request, 
 			@ModelAttribute ReferenceQueryForm query,
-			@ModelAttribute MySortPaginator mySort) {
+			@ModelAttribute Paginator page) {
 				
 		logger.debug(query.toString());
-		logger.debug("mysort: " + mySort.toString());
 		
 		SearchParams params = new SearchParams();
-		List<Sort> sorts = new ArrayList<Sort>();
-		
-		String sortStr = mySort.getSort();
-		if (sortStr == null || "".equals(sortStr)){
-			// set a default sort
-			mySort.setSort("journal");
-		}
-		
-		//this is interim code.  soon there will be one sort and/or paginator
-		// for now convert mySort to the object required by SearchParams
-		Sort sort = new Sort(mySort.getSort());
-		if (mySort.getDir().equals("asc")){
-			sort.setDesc(true);
-		} else {
-			sort.setDesc(false);
-		}
-		sorts.add(sort);
-		params.setSorts(sorts);
-		
-		params.setStartIndex(mySort.getStartIndex());
-		params.setPageSize(mySort.getResults());
-		
+		params.setPaginator(page);		
+		params.setSorts(this.parseSorts(request));
+		params.setFilter(this.parseReferenceQueryForm(query));
+
+		// perform query and return results as json
+		logger.debug("params parsed");
+		return referenceFinder.searchReferences(params);
+	}
+	
+	private Filter parseReferenceQueryForm(ReferenceQueryForm query){
 		// start filter list to add filters to
 		List<Filter> filterList = new ArrayList<Filter>();
 		
@@ -143,7 +132,7 @@ public class ReferenceController {
 		
 		// build text query filter
 		if(query.getText() != null && !"".equals(query.getText())){
-			Filter f = new Filter();
+			Filter tf = new Filter();
 			List<Filter> textFilters = new ArrayList<Filter>();
 			
 			String text = query.getText();
@@ -156,9 +145,9 @@ public class ReferenceController {
 			if (textFilters.size() == 1) {
 				filterList.add(textFilters.get(0));
 			} else {
-				f.setFilterJoinClause(Filter.FC_OR);
-				f.setNestedFilters(textFilters);
-				filterList.add(f);
+				tf.setFilterJoinClause(Filter.FC_OR);
+				tf.setNestedFilters(textFilters);
+				filterList.add(tf);
 			}
 		}
 		
@@ -167,19 +156,43 @@ public class ReferenceController {
 			Filter f = new Filter();
 			f.setFilterJoinClause(Filter.FC_AND);
 			f.setNestedFilters(filterList);
-			params.setFilter(f);
+			return f;
 		// none yet, so check the id query and build it
 		} else if (query.getId() != null && !"".equals(query.getId())){
-			List<String> ids = Arrays.asList(query.getId().split(";"));	
-			params.setFilter(new Filter(SearchConstants.REF_ID, ids, Filter.OP_EQUAL));
+			List<String> ids = Arrays.asList(query.getId().split(";"));
+			//TODO filter 'PMID:' from ids
+			return new Filter(SearchConstants.REF_ID, ids, Filter.OP_IN);
 		} else {
 			//TODO no query params
 		}
-		// perform query and return results as json
-		logger.debug("params parsed");
-		return referenceFinder.searchReferences(params);
+		return new Filter();
 	}
 	
+	private List<Sort> parseSorts(HttpServletRequest request) {
+		
+		List<Sort> sorts = new ArrayList<Sort>();
+		
+		String s = request.getParameter("sort");
+		String d = request.getParameter("dir");
+		boolean desc = false;
+		
+		if ("authors".equalsIgnoreCase(s)){
+			s = SortConstants.REF_AUTHORS;
+		} else if ("journal".equalsIgnoreCase(s)){
+			s = SortConstants.REF_JOURNAL;
+		} else {
+			s = SortConstants.REF_YEAR;
+		}
+		
+		if("desc".equalsIgnoreCase(d)){
+			desc = true;
+		}
+		Sort sort = new Sort(s, desc);
+		
+		sorts.add(sort);
+		return sorts;
+	}
+
 	// mapping for author autocomplete requests
 	@RequestMapping("/autocomplete/author")
 	public @ResponseBody SearchResults<String> authorAutoComplete(@RequestParam("query") String query) {
@@ -205,7 +218,7 @@ public class ReferenceController {
 		Filter f;
 		SearchParams params = new SearchParams();
 		params.setPageSize(1000);
-		params.setStartIndex(0);
+		//params.setStartIndex(0);
 		
 		if (queries.size() > 1){
 			f = new Filter();
