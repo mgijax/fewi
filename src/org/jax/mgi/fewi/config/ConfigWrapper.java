520adce8a5cda51ce9c307de698b5a880bd25712
Peter Frost <Pete.Frost@jax.org>
2010-10-12 13:57:46 +0000

    removal of injected values, now gathered via properties objects

--- src/org/jax/mgi/fewi/config/ConfigWrapper.java
+++ src/org/jax/mgi/fewi/config/ConfigWrapper.java
@@ -1,18 +1,68 @@
 package org.jax.mgi.fewi.config;
 
+import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConfigWrapper {
-	
-	private @Value("${solr.reference.url}") String solr;
-	
-	public String getSolr() {
-		return solr;
+
+
+    // property objects; will be loaded at construction
+    Properties fewiProperties = new Properties();   // fewi.properties
+    Properties globalProperties = new Properties(); // GlobalConfig.properties
+
+
+    @PostConstruct
+    public void init() {
+
+        // input streams used to read the properties files
+        InputStream gcPropertiesIn =
+          ConfigWrapper.class.getClassLoader().getResourceAsStream("../properties/GlobalConfig.properties");
+        InputStream fwPropertiesIn =
+          ConfigWrapper.class.getClassLoader().getResourceAsStream("../properties/fewi.properties");
+
+
+        try {globalProperties.load(gcPropertiesIn);}
+        catch (Exception e) {
+          System.out.println("--> ConfigWrapper - Error loading GlobalConfig.properties");
+        }
+
+        try {fewiProperties.load(fwPropertiesIn);}
+        catch (Exception e) {
+          System.out.println("--> ConfigWrapper - Error loading fewi.properties");
+        }
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //  accessors mapping file properties to bean-based access for view
+    ///////////////////////////////////////////////////////////////////////////
+
+    public String getFewiUrl() {
+		if (fewiProperties.getProperty("fewi.url") != null) {
+          return fewiProperties.getProperty("fewi.url");
+	    }
+        return globalProperties.getProperty("FEWI_URL");
+    }
+	public String getSeqFetchUrl() {
+		return globalProperties.getProperty("SEQFETCH_URL");
 	}
 
-	public String formatStr(String s){
-		return "your arg: " + s; 
-	}
 }
+
+
+
+///////////////////////////////////////////////////////////////////////////
+//  old code to be safed on the off chance we need switch implementations
+///////////////////////////////////////////////////////////////////////////
+//    private String fewiUrlFromGlobalConfig;
+//    @Value("#{GlobalConfig.FEWI_URL}")
+//    public void setFewiUrlGlobalConfig(String url) {
+//        this.fewiUrlFromGlobalConfig = url;
+//    }
+//    private @Value("${fewi.url}") String fewiUrlFromLocalConfig;
+
