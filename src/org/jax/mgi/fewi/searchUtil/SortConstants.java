fa5703ea476d534f00b092751f901806423a8ac4
Jonathan Beal <Jon.Beal@jax.org>
2010-10-21 15:34:15 +0000

    removed unnecessary ones; added CRE_DETECTED_COUNT

--- src/org/jax/mgi/fewi/searchUtil/SortConstants.java
+++ src/org/jax/mgi/fewi/searchUtil/SortConstants.java
@@ -15,30 +15,5 @@ public class SortConstants {
 	public static final String CRE_TYPE = "alleleType";
 	public static final String CRE_INDUCIBLE = "inducibleNote";
 	public static final String CRE_REF_COUNT = "countOfReferences";
-	public static final String CRE_DETECTED_IN = "detectedIn";
-	
-	public static final String CRE_IN_ADIPOSE_TISSUE           = "inAdiposeTissue";
-	public static final String CRE_IN_ALIMENTARY_SYSTEM        = "inAlimentarySystem";
-	public static final String CRE_IN_BRANCHIAL_ARCHES         = "inBranchialArches";
-	public static final String CRE_IN_CARDIOVASCULAR_SYSTEM    = "inCardiovascularSystem";
-	public static final String CRE_IN_CAVITIES_AND_LINININGS   = "inCavitiesAndLinings";
-	public static final String CRE_IN_ENDOCRINE_SYSTEM         = "inEndocrineSystem";
-	public static final String CRE_IN_HEAD                     = "inHead";
-	public static final String CRE_IN_HEMOLYMPHOID_SYSTEM      = "inHemolymphoidSystem";
-	public static final String CRE_IN_INTEGUMENTAL_SYSTEM      = "inIntegumentalSystem";
-	public static final String CRE_IN_LIMBS                    = "inLimbs";
-	public static final String CRE_IN_LIVER_AND_BILIARY_SYSTEM = "inLiverAndBiliarySystem";
-	public static final String CRE_IN_MESENCHYME               = "inMesenchyme";
-	public static final String CRE_IN_MUSCLE                   = "inMuscle";
-	public static final String CRE_IN_NERVOUS_SYSTEM           = "inNervousSystem";
-	public static final String CRE_IN_RENAL_AND_URINARY_SYSTEM = "inRenalAndUrinarySystem";
-	public static final String CRE_IN_REPRODUCTIVE_SYSTEM      = "inReproductiveSystem";
-	public static final String CRE_IN_RESPIRATORY_SYSTEM       = "inRespiratorySystem";
-	public static final String CRE_IN_SENSORY_ORGANS           = "inSensoryOrgans";
-	public static final String CRE_IN_SKELETAL_SYSTEM          = "inSkeletalSystem";
-	public static final String CRE_IN_TAIL                     = "inTail";
-	public static final String CRE_IN_EARLY_EMBRYO             = "inEarlyEmbryo";
-	public static final String CRE_IN_EXTRAEMBRYONIC_COMPONENT = "inExtraembryonicComponent";
-	public static final String CRE_IN_EMBRYO_OTHER             = "inEmbryoOther";
-	public static final String CRE_IN_POSTNATAL_OTHER          = "inPostnatalOther";
+	public static final String CRE_DETECTED_COUNT = "detectedIn";
 }
