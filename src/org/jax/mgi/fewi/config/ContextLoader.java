40b91676610f87920f7a2bc4a698696b5ce7ff6f
Mark Airey <Mark.Airey@jax.org>
2010-11-03 14:57:33 +0000

    allow configBean access via static method

--- src/org/jax/mgi/fewi/config/ContextLoader.java
+++ src/org/jax/mgi/fewi/config/ContextLoader.java
@@ -1,31 +1,54 @@
 package org.jax.mgi.fewi.config;
 
+import java.util.Properties;
+
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 
 import org.jax.mgi.fewi.template.WebTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.ServletContextAware;
 
 @Component
-public class ContextLoader implements ServletContextAware {
+public class ContextLoader implements ApplicationContextAware, ServletContextAware {
+	
+	private ApplicationContext ac;
+	private ServletContext sc;
+	
+	private static Properties properties = null;
 
 	private Logger logger = LoggerFactory.getLogger(ContextLoader.class);
-    private ServletContext servletContext;
 
     @Autowired
     private WebTemplate webTemplate;
 
-	public void setServletContext(ServletContext servletContext) {
-		this.servletContext = servletContext;
-	}
 
 	@PostConstruct
     public void init() {
-            servletContext.setAttribute("templateBean", webTemplate);
-            logger.debug("configs loaded");
+        sc.setAttribute("templateBean", webTemplate);
+        logger.debug("configs loaded");
+		if(ac.containsBean("configBean")){
+			properties =  (Properties)ac.getBean("configBean");
+		}
     }
+	
+	public static Properties getConfigBean(String beanName){
+		return properties;
+	}
+
+	public void setApplicationContext(ApplicationContext ac)
+			throws BeansException {
+		this.ac = ac;
+		
+	}
+
+	public void setServletContext(ServletContext sc) {
+		this.sc = sc;
+	}
 }
