7ae73b2f79004a2c9d819cdd56768b67861cb2f7
Peter Frost <Pete.Frost@jax.org>
2010-11-04 13:13:27 +0000

    Initial version

---
+++ src/org/jax/mgi/fewi/util/DBConstants.java
@@ -0,0 +1,29 @@
+package org.jax.mgi.fewi.util;
+
+/**
+* DBConstants.java
+*/
+
+/** gives names for common database entries, to make code more
+*    readable and maintainable.
+*/
+public class DBConstants
+{
+    // PROVIDERS
+    public static final String PROVIDER_DFCI              = "DFCI Mouse Gene Index";
+    public static final String PROVIDER_NCBI              = "NCBI Gene Model";
+    public static final String PROVIDER_DOTS              = "DoTS";
+    public static final String PROVIDER_TREMBL            = "TrEMBL";
+    public static final String PROVIDER_SEQUENCEDB        = "GenBank/EMBL/DDBJ";
+    public static final String PROVIDER_SWISSPROT         = "SWISS-PROT";
+    public static final String PROVIDER_NIA               = "NIA Mouse Gene Index";
+    public static final String PROVIDER_REFSEQ            = "RefSeq";
+    public static final String PROVIDER_ENSEMBL           = "Ensembl Gene Model";
+    public static final String PROVIDER_ENSEMBLPROTEIN    = "Ensembl Protein";
+    public static final String PROVIDER_ENSEMBLTRANSCRIPT = "Ensembl Transcript";
+    public static final String PROVIDER_VEGA              = "VEGA Gene Model";
+    public static final String PROVIDER_VEGATRANSCRIPT    = "VEGA Transcript";
+    public static final String PROVIDER_VEGAPROTEIN       = "VEGA Protein";
+
+}
+
