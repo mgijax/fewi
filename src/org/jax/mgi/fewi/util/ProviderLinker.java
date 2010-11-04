8205ecb44a04f1ae8066c27e49b72a8714ab4734
Peter Frost <Pete.Frost@jax.org>
2010-11-04 13:22:17 +0000

    Updated to use constants

--- src/org/jax/mgi/fewi/util/ProviderLinker.java
+++ src/org/jax/mgi/fewi/util/ProviderLinker.java
@@ -1,5 +1,7 @@
 package org.jax.mgi.fewi.util;
 
+import org.jax.mgi.fewi.util.DBConstants;
+
 import java.util.*;
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,11 +36,10 @@ public class ProviderLinker
 	private static String ncbiGmUrl = "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?db=gene&cmd=Retrieve&dopt=Graphics&list_uids=";
 	private static String niaUrl = "http://lgsun.grc.nia.nih.gov/geneindex/mm9/bin/giU.cgi?genename=";
 	private static String refSeqUrl = "http://www.ncbi.nlm.nih.gov/entrez/viewer.cgi?val=";
-	private static String vegaGmUrl = "";
-	private static String vegaProtUrl = "";
+	private static String vegaGmUrl = "http://vega.sanger.ac.uk/Mus_musculus/Gene/Summary?db=core;g=";
+	private static String vegaProtUrl = "http://vega.sanger.ac.uk/Mus_musculus/Transcript/ProteinSummary?db=core;t=";
 	private static String vegaTranUrl = "http://vega.sanger.ac.uk/Mus_musculus/Transcript/Summary?db=core;t=";
-
-
+	private static String dotsUrl = "	http://genomics.betacell.org/gbco/showSummary.do?questionFullName=TranscriptQuestions.TranscriptFromDtIds&myProp%28dtIdP%29=";
 
 
 //TODO - move provider string test to use constants
@@ -62,57 +63,62 @@ public class ProviderLinker
 
 
         // all genbank
-        if (seqProvider.startsWith("GenBank/EMBL/DDBJ")) {
+        if (seqProvider.startsWith(DBConstants.PROVIDER_SEQUENCEDB)) {
 
 			links.append("(<a href='" + genBankUrl + seqID + "'>GenBank</a> | ");
 			links.append("<a href='" + emblUrl + seqID + "'>EMBL</a> | ");
 			links.append("<a href='" + ddbjUrl + seqID + "'>DDBJ</a>)");
 		}
         // DFCI
-        else if (seqProvider.equals("DFCI Mouse Gene Index")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_DFCI)) {
 
 			links.append("(<a href='" + dfciUrl + seqID + "'>DFCI</a>)");
 		}
-        else if (seqProvider.equals("Ensembl Gene Model")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_ENSEMBL)) {
 
 			links.append("(<a href='" + ensemblGmUrl + seqID + "'>Ensembl Gene Model</a>)");
 		}
-        else if (seqProvider.equals("Ensembl Protein")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_ENSEMBLPROTEIN)) {
 
 			links.append("(<a href='" + ensemblProtUrl + seqID + "'>Ensembl</a>)");
 		}
-        else if (seqProvider.equals("Ensembl Transcript")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_ENSEMBLTRANSCRIPT)) {
 
 			links.append("(<a href='" + ensemblTranUrl + seqID + "'>Ensembl</a>)");
 		}
-        else if (seqProvider.equals("TrEMBL") || seqProvider.equals("SWISS-PROT")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_TREMBL) ||
+                 seqProvider.equals(DBConstants.PROVIDER_SWISSPROT)) {
 
 			links.append("(<a href='" + uniProtUrl + seqID + "'>UniProt</a> | ");
 			links.append("<a href='" + ebiUrl + seqID + "'>EBI</a>)");
 		}
-        else if (seqProvider.equals("NCBI Gene Model")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_NCBI)) {
 
 			links.append("(<a href='" + ncbiGmUrl + seqID + "'>NCBI Gene Model</a>)");
 		}
-        else if (seqProvider.equals("NIA Mouse Gene Index")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_NIA)) {
 
 			links.append("(<a href='" + niaUrl + seqID + "'>NIA</a>)");
 		}
-        else if (seqProvider.equals("RefSeq")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_REFSEQ)) {
 
 			links.append("(<a href='" + refSeqUrl + seqID + "'>RefSeq</a>)");
 		}
-        else if (seqProvider.equals("VEGA Transcript")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_VEGATRANSCRIPT)) {
 
 			links.append("(<a href='" + vegaTranUrl + seqID + "'>VEGA</a>)");
 		}
-        else if (seqProvider.equals("VEGA Gene Model")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_VEGA)) {
 
 			links.append("(<a href='" + vegaGmUrl + seqID + "'>VEGA</a>)");
 		}
-        else if (seqProvider.equals("VEGA Protein")) {
+        else if (seqProvider.equals(DBConstants.PROVIDER_VEGAPROTEIN)) {
 
 			links.append("(<a href='" + vegaProtUrl + seqID + "'>VEGA</a>)");
+		}
+        else if (seqProvider.equals(DBConstants.PROVIDER_DOTS)) {
+
+			links.append("(<a href='" + dotsUrl + seqID + "'>DoTS</a>)");
 		}
 
 
