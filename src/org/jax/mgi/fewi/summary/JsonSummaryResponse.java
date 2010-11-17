ed016bda940e2b7ad2d9d3227c3dee8f40cb8f5f
Peter Frost <Pete.Frost@jax.org>
2010-11-17 17:52:38 +0000

    Initial version

---
+++ src/org/jax/mgi/fewi/summary/JsonSummaryResponse.java
@@ -0,0 +1,57 @@
+package org.jax.mgi.fewi.searchUtil;
+
+import java.util.*;
+
+/**
+ * JSON Response Object;  the purpose of this object is to be serialized
+ * into a JSON response for YUI datatable interaction
+ */
+public class JsonSummaryResponse<T> {
+
+
+    //////////////////////////////////////////////////////////////////////////
+    //  INTERNAL FIELDS
+    //////////////////////////////////////////////////////////////////////////
+
+	// Result Objects of a given search
+	protected List<T> summaryRows = new ArrayList<T>();
+
+	// Total number of possible results
+	protected int totalCount = -1;
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    //  BASIC ACCESSORS
+    //////////////////////////////////////////////////////////////////////////
+
+
+	/**
+	 * Get the summary rows
+	 */
+	public List<T> getSummaryRows() {
+		return summaryRows;
+	}
+
+	/**
+	 * Set the summary rows
+	 */
+	public void setSummaryRows(List<T> summaryRows) {
+		this.summaryRows = summaryRows;
+	}
+
+
+    /**
+	 * Get the total number of possible results (-1 means unspecified)
+	 */
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	/**
+	 * Set the total number of possible results
+	 */
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+}
