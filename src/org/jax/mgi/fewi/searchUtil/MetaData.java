db6bc32dadc9bdc38d7e6c4633c36f04096a16e0
Matt Hall <Matt.Hall@jax.org>
2010-12-22 18:50:16 +0000

    Initial version of the MetaData object

---
+++ src/org/jax/mgi/fewi/searchUtil/MetaData.java
@@ -0,0 +1,26 @@
+package org.jax.mgi.fewi.searchUtil;
+
+import java.util.Set;
+
+public class MetaData {
+    
+    String score;
+    Set <String> highlightWords;
+    
+    public String getScore() {
+        return score;
+    }
+    public void setScore(String score) {
+        this.score = score;
+    }
+    public Set<String> getHighlightWords() {
+        return highlightWords;
+    }
+    public void setHighlightWords(Set<String> highlightWords) {
+        this.highlightWords = highlightWords;
+    }
+    public void addHighlightWords(String word) {
+        this.highlightWords.add(word);
+    }
+
+}
