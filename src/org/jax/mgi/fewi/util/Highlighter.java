cc75c45c2f2010575b9c664e083bf4984c2fa94a
Mark Airey <Mark.Airey@jax.org>
2011-01-12 19:45:35 +0000

    migrating to new MetaData objects

--- src/org/jax/mgi/fewi/util/Highlighter.java
+++ src/org/jax/mgi/fewi/util/Highlighter.java
@@ -1,5 +1,6 @@
 package org.jax.mgi.fewi.util;
 
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,14 +12,14 @@ public class Highlighter {
     private String highlightTag = "span";
     private String highlightClass = "highlight";
 
-    public Highlighter(String[] toFind) {
+    public Highlighter(Collection<String> toFind) {
 	    // build reg expr pattern string like:
 	    //  "\\b(word1|word2|word3|...)"
 	    // (could make the \\b an option so we could highlight text that
 	    //   doesn't start on a word boundary)
 	    String patternStr;	    
     		
-    	if (toFind != null && !"".equals(toFind)){
+    	if (toFind != null){
     		patternStr = "\\b(" + StringUtils.join(toFind, "|") + ")\\b";
     	    pattern = Pattern.compile( patternStr, Pattern.CASE_INSENSITIVE);
     	}
