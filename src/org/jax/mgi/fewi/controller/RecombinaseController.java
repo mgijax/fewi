0f1ea093edb8c2c5dd74ee9277b60055f9cdb4db
Peter Frost <Pete.Frost@jax.org>
2011-02-10 13:38:32 +0000

    Updates

--- src/org/jax/mgi/fewi/controller/RecombinaseController.java
+++ src/org/jax/mgi/fewi/controller/RecombinaseController.java
@@ -5,9 +5,14 @@ package org.jax.mgi.fewi.controller;
 /*------------------------------*/
 
 import mgi.frontend.datamodel.Allele;
+import mgi.frontend.datamodel.AlleleSystem;
+import mgi.frontend.datamodel.AlleleSynonym;
+import mgi.frontend.datamodel.AlleleSystemAssayResult;
+import mgi.frontend.datamodel.Reference;
 import org.jax.mgi.fewi.finder.RecombinaseFinder;
 import org.jax.mgi.fewi.forms.RecombinaseQueryForm;
 import org.jax.mgi.fewi.summary.RecombinaseSummary;
+import org.jax.mgi.fewi.summary.RecomSpecificitySummaryRow;
 import org.jax.mgi.fewi.summary.JsonSummaryResponse;
 import java.util.Iterator;
 
@@ -15,10 +20,7 @@ import java.util.Iterator;
 /* standard imports for all controllers */
 /*--------------------------------------*/
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 import org.jax.mgi.fewi.searchUtil.Filter;
 import org.jax.mgi.fewi.searchUtil.Paginator;
 import org.jax.mgi.fewi.searchUtil.SearchConstants;
@@ -26,16 +28,23 @@ import org.jax.mgi.fewi.searchUtil.SearchParams;
 import org.jax.mgi.fewi.searchUtil.SearchResults;
 import org.jax.mgi.fewi.searchUtil.Sort;
 import org.jax.mgi.fewi.searchUtil.SortConstants;
+import org.jax.mgi.fewi.util.FormatHelper;
+
+// external
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 
 /*-------*/
 /* class */
@@ -51,11 +60,11 @@ public class RecombinaseController {
 	/*--------------------*/
 	/* instance variables */
 	/*--------------------*/
-	
+
 	// logger for the class
 	private Logger logger = LoggerFactory.getLogger (
 		RecombinaseController.class);
-	
+
 	// get the finder to use for various methods
 	@Autowired
 	private RecombinaseFinder recombinaseFinder;
@@ -64,48 +73,56 @@ public class RecombinaseController {
 	/* public instance methods */
 	/*-------------------------*/
 
-	// add new RecombinaseQueryForm and Paginator objects to model for QF 
+	// add new RecombinaseQueryForm and Paginator objects to model for QF
 	@RequestMapping(method=RequestMethod.GET)
 	public String getQueryForm(Model model) {
 		model.addAttribute(new RecombinaseQueryForm());
 		model.addAttribute("sort", new Paginator());
 		return "recombinase_query";
 	}
-	
+
 	// qf submission.  drop completed ReferenceQueryForm object and query string
 	// into model for summary to use
+    //-------------------------------//
+    // Query Form Submission
+    //-------------------------------//
 	@RequestMapping("/summary")
 	public String recombinaseSummary(HttpServletRequest request, Model model,
 			@ModelAttribute RecombinaseQueryForm queryForm) {
 
+		logger.debug("queryString: " + request.getQueryString());
+
+		// objects needed by display
 		model.addAttribute("recombinaseQueryForm", queryForm);
 		model.addAttribute("queryString", request.getQueryString());
-		logger.debug("queryString: " + request.getQueryString());
 
 		return "recombinase_summary";
 	}
-	
+
+    //-------------------------------//
+    // JSON Results for Summary
+    //-------------------------------//
 	// this is the logic to perform the query and return json results
 	@RequestMapping("/json")
 	public @ResponseBody JsonSummaryResponse<RecombinaseSummary> recombinaseSummaryJson(
-			HttpServletRequest request, 
+			HttpServletRequest request,
 			@ModelAttribute RecombinaseQueryForm query,
 			@ModelAttribute Paginator page) {
-				
+
 		logger.debug(query.toString());
 
 		// set up search parameters
-		
+
 		SearchParams params = new SearchParams();
-		params.setPaginator(page);		
+		params.setPaginator(page);
 		params.setSorts(this.parseSorts(request));
 		params.setFilter(this.parseRecombinaseQueryForm(query));
 
 		// issue the query and get back the matching Allele objects
-		
-		SearchResults<Allele> searchResults = 
+
+		SearchResults<Allele> searchResults =
 			recombinaseFinder.searchRecombinases(params);
-		
+
 		// convert the Alleles to their RecombinaseSummary wrappers, and put
 		// them in the JsonSummaryResponse object
 
@@ -115,30 +132,140 @@ public class RecombinaseController {
 			summaries.add(new RecombinaseSummary(it.next()));
 		}
 
-        JsonSummaryResponse<RecombinaseSummary> jsonResponse = 
+        JsonSummaryResponse<RecombinaseSummary> jsonResponse =
         	new JsonSummaryResponse<RecombinaseSummary>();
 
         jsonResponse.setSummaryRows (summaries);
         jsonResponse.setTotalCount (searchResults.getTotalCount());
-		
+
 		return jsonResponse;
 	}
 
-	/*--------------------------*/
-	/* private instance methods */
-	/*--------------------------*/
+
+
+    //-------------------------------//
+    // Cre Specificity
+    //-------------------------------//
+	@RequestMapping("/specificity")
+    public ModelAndView creSpecificity(
+		    HttpServletRequest request,
+			@ModelAttribute RecombinaseQueryForm query) {
+
+        logger.debug("->creSpecificity() started");
+
+        ModelAndView mav = new ModelAndView("recombinase_specificity");
+
+        // setup search parameters object to gather the requested object
+        SearchParams searchParams = new SearchParams();
+        searchParams.setFilter(this.genFilters(query));
+
+        // find the requested allele/system object
+		SearchResults<AlleleSystem> searchResults =
+			recombinaseFinder.getAlleleSystem(searchParams);
+        List<AlleleSystem> alleleSystems = searchResults.getResultObjects();
+
+        // ensure we found something...
+        if (alleleSystems.size() < 1) {
+            // forward to error page
+            mav = new ModelAndView("error");
+            mav.addObject("errorMsg", "Allele/System not available");
+            return mav;
+        }
+
+        // gather required data objects, and place into the mav
+        AlleleSystem alleleSystem = alleleSystems.get(0);
+        Allele allele = alleleSystem.getAllele();
+        mav.addObject("alleleSystem", alleleSystem);
+        mav.addObject("allele", allele);
+        mav.addObject("systemDisplayStr",
+          FormatHelper.initCap(alleleSystem.getSystem()));
+        mav.addObject("galleryImages", alleleSystem.getImages());
+        mav.addObject("otherAlleles", alleleSystem.getOtherAlleles());
+        mav.addObject("otherSystems", alleleSystem.getOtherSystems());
+
+        // pre-gen comma-delimitted synonym list
+		List<String> synonymList = new ArrayList<String> ();
+		Iterator<AlleleSynonym> synonymIter = allele.getSynonyms().iterator();
+		while (synonymIter.hasNext()) {
+			AlleleSynonym thisSynonym = synonymIter.next();
+			synonymList.add(thisSynonym.getSynonym());
+		}
+        mav.addObject("synonymsString",
+          FormatHelper.superscript(FormatHelper.commaDelimit(synonymList)));
+
+		// add query string
+		mav.addObject("queryString", request.getQueryString());
+
+
+        return mav;
+    }
+
+
+
+    //----------------------//
+    // JSON summary results
+    //----------------------//
+    @RequestMapping("/jsonSpecificity")
+    public @ResponseBody JsonSummaryResponse<RecomSpecificitySummaryRow> specificitySummaryJson(
+            HttpServletRequest request,
+			@ModelAttribute RecombinaseQueryForm query,
+            @ModelAttribute Paginator page) {
+
+        logger.debug("->specificitySummaryJson started");
+
+        // generate search parms object;  add pagination, sorts, and filters
+        SearchParams params = new SearchParams();
+        params.setPaginator(page);
+//        params.setSorts(this.genSorts(request));
+        params.setFilter(this.genFilters(query));
+
+        // perform query, and pull out the requested objects
+		SearchResults<AlleleSystemAssayResult> searchResults =
+			recombinaseFinder.getAssaySummary(params);
+        List<AlleleSystemAssayResult> assayResultList
+          = searchResults.getResultObjects();
+
+        // create/load the list of SummaryRow wrapper objects
+        List<RecomSpecificitySummaryRow> summaryRows
+          = new ArrayList<RecomSpecificitySummaryRow> ();
+        Iterator<AlleleSystemAssayResult> it = assayResultList.iterator();
+        while (it.hasNext()) {
+            AlleleSystemAssayResult thisAssayResult = it.next();
+            if (thisAssayResult == null) {
+                logger.debug("--> Null Object");
+            }else {
+                summaryRows.add(new RecomSpecificitySummaryRow(thisAssayResult));
+            }
+        }
+
+        // The JSON return object will be serialized to a JSON response.
+        // Client-side JavaScript expects this object
+        JsonSummaryResponse<RecomSpecificitySummaryRow> jsonResponse
+          = new JsonSummaryResponse<RecomSpecificitySummaryRow>();
+
+        // place data into JSON response, and return
+        jsonResponse.setSummaryRows(summaryRows);
+        jsonResponse.setTotalCount(searchResults.getTotalCount());
+        return jsonResponse;
+    }
+
+
+
+	/*---------------------------------------------------------------------*/
+	/* private instance methods                                            */
+	/*---------------------------------------------------------------------*/
 
 	/** TODO : needs to be adjusted for alleles
-	 * 
+	 *
 	 */
 	private List<Sort> parseSorts(HttpServletRequest request) {
-		
+
 		List<Sort> sorts = new ArrayList<Sort>();
-		
+
 		String s = request.getParameter("sort");
 		String d = request.getParameter("dir");
 		boolean desc = false;
-/*		
+/*
 		if ("symbol".equalsIgnoreCase(s)){
 			s = SortConstants.CRE_SYMBOL;
 		} else if ("alleletype".equalsIgnoreCase(s)){
@@ -154,7 +281,7 @@ public class RecombinaseController {
 		}
 */
 		if (s == null) { s = SortConstants.CRE_DRIVER; }
-		
+
 		if("desc".equalsIgnoreCase(d)){
 			desc = true;
 		}
@@ -167,22 +294,22 @@ public class RecombinaseController {
 	private Filter parseRecombinaseQueryForm(RecombinaseQueryForm query){
 		// start filter list to add filters to
 		List<Filter> filterList = new ArrayList<Filter>();
-		
+
 		String driver = query.getDriver();
 		String system = query.getSystem();
-		
+
 		// build driver query filter
 		if ((driver != null) && (!"".equals(driver))) {
-			filterList.add(new Filter (SearchConstants.ALL_DRIVER, driver, 
+			filterList.add(new Filter (SearchConstants.ALL_DRIVER, driver,
 				Filter.OP_EQUAL));
 		}
-		
+
 		// build system query filter
 		if ((system != null) && (!"".equals(system))) {
 			filterList.add(new Filter (SearchConstants.ALL_SYSTEM, system,
 				Filter.OP_EQUAL));
 		}
-		
+
 		// we do have some filters, build 'em and add to searchParams
 		if (filterList.size() > 0){
 			Filter f = new Filter();
@@ -199,4 +326,53 @@ public class RecombinaseController {
 		}
 		return new Filter();
 	}
+
+
+
+
+    // generation of filters
+    private Filter genFilters(RecombinaseQueryForm query){
+
+        logger.debug("->genFilters started");
+        logger.debug("QueryForm -> " + query);
+
+        // start filter list to add filters to
+        List<Filter> filterList = new ArrayList<Filter>();
+
+		String driver = query.getDriver();
+		String system = query.getSystem();
+		String id = query.getId();
+		String systemKey = query.getSystemKey();
+
+		// build the possible filters
+		if ((driver != null) && (!"".equals(driver))) {
+			filterList.add(new Filter (SearchConstants.ALL_DRIVER, driver,
+				Filter.OP_EQUAL));
+		}
+		if ((system != null) && (!"".equals(system))) {
+			filterList.add(new Filter (SearchConstants.ALL_SYSTEM, system,
+				Filter.OP_EQUAL));
+		}
+		if ((id != null) && (!"".equals(id))) {
+			filterList.add(new Filter (SearchConstants.ALL_ID, id,
+				Filter.OP_EQUAL));
+		}
+		if ((systemKey != null) && (!"".equals(systemKey))) {
+			filterList.add(new Filter (SearchConstants.CRE_SYSTEM_KEY, systemKey,
+				Filter.OP_EQUAL));
+		}
+
+        // if we have filters, collapse them into a single filter
+        Filter containerFilter = new Filter();
+        if (filterList.size() > 0){
+
+            containerFilter.setFilterJoinClause(Filter.FC_AND);
+            containerFilter.setNestedFilters(filterList);
+        }
+
+        logger.debug("genFilters -> " + containerFilter);
+        return containerFilter;
+    }
+
+
 }
--- src/org/jax/mgi/fewi/forms/RecombinaseQueryForm.java
+++ src/org/jax/mgi/fewi/forms/RecombinaseQueryForm.java
@@ -2,9 +2,11 @@ package org.jax.mgi.fewi.forms;
 
 
 public class RecombinaseQueryForm {
-	
+
 	private String driver;
+	private String id;
 	private String system;
+	private String systemKey;
 
 	public String getDriver() {
 		return driver;
@@ -12,17 +14,31 @@ public class RecombinaseQueryForm {
 	public void setDriver(String driver) {
 		this.driver = driver;
 	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 	public String getSystem() {
 		return system;
 	}
 	public void setSystem(String system) {
 		this.system = system;
 	}
-	
+	public String getSystemKey() {
+		return systemKey;
+	}
+	public void setSystemKey(String systemKey) {
+		this.systemKey = systemKey;
+	}
+
 	@Override
 	public String toString() {
 		return "RecombinaseQueryForm ["
 				+ (driver != null ? "driver=" + driver + ", " : "")
+				+ (id != null ? "id=" + id + ", " : "")
+				+ (systemKey != null ? "systemKey=" + systemKey + ", " : "")
 				+ (system != null ? "system=" + system : "") + "]";
-	}	
+	}
 }
