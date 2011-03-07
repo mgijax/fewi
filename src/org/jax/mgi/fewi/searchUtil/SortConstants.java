e8b95654b6ed2ae40afdc34b11c89f59ebf813b7
Matt Hall <Matt.Hall@jax.org>
2011-03-07 14:27:17 +0000

    Added a new author sort

--- src/org/jax/mgi/fewi/searchUtil/SortConstants.java
+++ src/org/jax/mgi/fewi/searchUtil/SortConstants.java
@@ -6,6 +6,7 @@ public class SortConstants {
 
 	public static final String REF_JOURNAL         = "journal";
 	public static final String REF_AUTHORS         = "authors";
+	public static final String REF_BY_AUTHOR       = "authorSort";
 	public static final String REF_YEAR            = "year";
 
 	// recombinase summary page
