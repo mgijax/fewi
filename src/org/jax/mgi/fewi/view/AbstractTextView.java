5bcd8b5d02320fd7e3d9f82e0a96bbf84c82c8fa
Mark Airey <Mark.Airey@jax.org>
2011-03-17 12:59:17 +0000

    adding report configuration wiring for reference report

---
+++ src/org/jax/mgi/fewi/view/AbstractTextView.java
@@ -0,0 +1,47 @@
+package org.jax.mgi.fewi.view;
+
+import java.io.BufferedWriter;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.view.AbstractView;
+
+public abstract class AbstractTextView extends AbstractView {
+	
+	// logger for the class
+	private Logger logger = LoggerFactory.getLogger(AbstractTextView.class);
+	
+	private static final String CONTENT_TYPE = "text/plain";
+	
+	AbstractTextView(){
+		super();
+		setContentType(CONTENT_TYPE);
+	}
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model,
+			HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		logger.debug("renderMergedOutputModel");
+	
+		
+		BufferedWriter writer = new BufferedWriter(response.getWriter());
+		
+		// Build Text document.
+		buildTextDocument(model, writer, request, response);
+		
+		// Flush to HTTP response.
+		writer.flush(); 
+		writer.close();
+	
+	}
+	
+	protected abstract void buildTextDocument(Map<String, Object> model, BufferedWriter writer,
+			HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+
+}
---
+++ src/org/jax/mgi/fewi/view/TextReferenceSummary.java
@@ -0,0 +1,30 @@
+package org.jax.mgi.fewi.view;
+
+import java.io.BufferedWriter;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mgi.frontend.datamodel.Reference;
+
+public class TextReferenceSummary extends AbstractTextView {
+
+	@Override
+	protected void buildTextDocument(Map<String, Object> model, BufferedWriter writer,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		
+		response.setHeader("Content-Disposition","attachment; filename=\"referenceReport.txt\"");
+		
+		List<Reference> references = (List<Reference>) model.get("results");
+		
+		for (Reference ref : references) {
+			writer.write(ref.getLongCitation()); 
+			writer.newLine();
+		}
+
+	}
+
+}
