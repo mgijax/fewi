a5cd7f28995d1c88216ea0cac6d1e04f3c9a9d67
Mark Airey <Mark.Airey@jax.org>
2011-03-31 14:28:02 +0000

    pulled this from mhall home directory as he forgot to commit it and fewi would no longer comiple

---
+++ src/org/jax/mgi/fewi/searchUtil/ObjectTypes.java
@@ -0,0 +1,14 @@
+package org.jax.mgi.fewi.searchUtil;
+
+public class ObjectTypes {
+	
+	public static String REFERENCE = "Reference";
+	public static String MARKER = "Marker";
+	public static String ALLELE	= "Allele";
+	public static String SEQUENCE = "Sequence";
+	public static String PROBECLONE = "Probe/Clone";
+	public static String IMAGE = "Image";
+	public static String ASSAY = "Assay";
+	public static String ORTHOLOGY = "Orthology";
+
+}
