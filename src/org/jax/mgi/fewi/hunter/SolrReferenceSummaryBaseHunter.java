bc7e1e051219d01d1273bccc8eeb426362f5c9e1
Matt Hall <Matt.Hall@jax.org>
2011-04-15 12:19:17 +0000

    Code review worthy version.

--- src/org/jax/mgi/fewi/hunter/SolrReferenceSummaryBaseHunter.java
+++ src/org/jax/mgi/fewi/hunter/SolrReferenceSummaryBaseHunter.java
@@ -18,65 +18,116 @@ import org.springframework.stereotype.Component;
 public class SolrReferenceSummaryBaseHunter extends SolrHunter {
     
     /***
-     * The constructor sets up this hunter so that it is specific to sequence
+     * The constructor sets up this hunter so that it is specific to reference
      * summary pages.  Each item in the constructor sets a value that it has 
      * inherited from its superclass, and then relies on the superclass to 
      * perform all of the needed work via the hunt() method.
      * 
-     * This class is the base for all of the other summary hunters.
+     * This class is the base for all of the other reference summary hunters.
      */
 	
     public SolrReferenceSummaryBaseHunter() {        
-        /**
-         * Set up the sorting filter mapping. 
-         */       
-        sortMap.put(SortConstants.REF_AUTHORS, new SolrSortMapper(IndexConstants.REF_AUTHOR));
-        sortMap.put(SortConstants.REF_JOURNAL, new SolrSortMapper(IndexConstants.REF_JOURNAL));
-        sortMap.put(SortConstants.REF_YEAR, new SolrSortMapper(IndexConstants.REF_YEAR));
+        
+    	/**
+         * Set up the sorting filter mapping.
+         * Technically this mapping is no longer strictly required, as 
+         * long as the requested sort column happens to match the sorting 
+         * column in the index. 
+         */
+    	
+        sortMap.put(SortConstants.REF_AUTHORS, 
+        		new SolrSortMapper(IndexConstants.REF_AUTHOR));
+        sortMap.put(SortConstants.REF_JOURNAL, 
+        		new SolrSortMapper(IndexConstants.REF_JOURNAL));
+        sortMap.put(SortConstants.REF_YEAR, 
+        		new SolrSortMapper(IndexConstants.REF_YEAR));
         
         /**
-         * Setup the property map.  This maps from the properties of the incoming
-         * filter list to the corresponding field names in the Solr implementation.
-         * 
-         */      
+         * Setup the property map.  This maps from the properties of the 
+         * incoming filter list to the corresponding field names in the Solr 
+         * implementation.
+         */
+        
+        /*
+         * This is a 1 -> N Relationship, a single field on the QF maps
+         * to multiple fields in the index.
+         */
+        
         ArrayList <String> refList = new ArrayList <String> ();
         refList.add(IndexConstants.JNUM_ID);
         refList.add(IndexConstants.PUBMED_ID);
         
-        propertyMap.put(SearchConstants.REF_ID, new SolrPropertyMapper(refList, "OR"));
-        propertyMap.put(SearchConstants.REF_AUTHOR_ANY, new SolrPropertyMapper(IndexConstants.REF_AUTHOR_FORMATTED));
-        propertyMap.put(SearchConstants.REF_AUTHOR_FIRST, new SolrPropertyMapper(IndexConstants.REF_FIRST_AUTHOR));
-        propertyMap.put(SearchConstants.REF_AUTHOR_LAST, new SolrPropertyMapper(IndexConstants.REF_LAST_AUTHOR));
-        propertyMap.put(SearchConstants.REF_JOURNAL, new SolrPropertyMapper(IndexConstants.REF_JOURNAL));
-        propertyMap.put(SearchConstants.REF_YEAR, new SolrPropertyMapper(IndexConstants.REF_YEAR));
-        propertyMap.put(SearchConstants.SEQ_KEY, new SolrPropertyMapper(IndexConstants.SEQ_KEY));
-        propertyMap.put(SearchConstants.ALL_KEY, new SolrPropertyMapper(IndexConstants.ALL_KEY));
-        propertyMap.put(SearchConstants.MRK_KEY, new SolrPropertyMapper(IndexConstants.MRK_KEY));
+        // The 1->N Mapping should be joined with OR's
+        propertyMap.put(SearchConstants.REF_ID, 
+        		new SolrPropertyMapper(refList, "OR"));
         
-        ArrayList <String> titleList = new ArrayList <String> ();
-        titleList.add(IndexConstants.REF_TITLE_STEMMED);
-        titleList.add(IndexConstants.REF_TITLE_UNSTEMMED);
+        propertyMap.put(SearchConstants.REF_AUTHOR_ANY, 
+        		new SolrPropertyMapper(IndexConstants.REF_AUTHOR_FORMATTED));
+        propertyMap.put(SearchConstants.REF_AUTHOR_FIRST, 
+        		new SolrPropertyMapper(IndexConstants.REF_FIRST_AUTHOR));
+        propertyMap.put(SearchConstants.REF_AUTHOR_LAST, 
+        		new SolrPropertyMapper(IndexConstants.REF_LAST_AUTHOR));
+        propertyMap.put(SearchConstants.REF_JOURNAL, 
+        		new SolrPropertyMapper(IndexConstants.REF_JOURNAL));
+        propertyMap.put(SearchConstants.REF_YEAR, 
+        		new SolrPropertyMapper(IndexConstants.REF_YEAR));
+        propertyMap.put(SearchConstants.SEQ_KEY, 
+        		new SolrPropertyMapper(IndexConstants.SEQ_KEY));
+        propertyMap.put(SearchConstants.ALL_KEY, 
+        		new SolrPropertyMapper(IndexConstants.ALL_KEY));
+        propertyMap.put(SearchConstants.MRK_KEY, 
+        		new SolrPropertyMapper(IndexConstants.MRK_KEY));
+
+        /*
+         *  This is a 1->N Relationship from QF to index fields
+         */
         
         ArrayList <String> abstractList = new ArrayList <String> ();
         abstractList.add(IndexConstants.REF_ABSTRACT_STEMMED);
         abstractList.add(IndexConstants.REF_ABSTRACT_UNSTEMMED);
         
-        propertyMap.put(SearchConstants.REF_TEXT_ABSTRACT, new SolrReferenceTextSearchPropertyMapper(abstractList, "OR"));
-        propertyMap.put(SearchConstants.REF_TEXT_TITLE, new SolrReferenceTextSearchPropertyMapper(titleList, "OR"));
+        propertyMap.put(SearchConstants.REF_TEXT_ABSTRACT, 
+        		new SolrReferenceTextSearchPropertyMapper(abstractList, "OR"));
+
+        /*
+         * This is a 1->N Relationship from QF to index fields
+         */
         
+        ArrayList <String> titleList = new ArrayList <String> ();
+        titleList.add(IndexConstants.REF_TITLE_STEMMED);
+        titleList.add(IndexConstants.REF_TITLE_UNSTEMMED);
+        
+        // The 1->N Mapping should be joined with OR's
+        propertyMap.put(SearchConstants.REF_TEXT_TITLE, 
+        		new SolrReferenceTextSearchPropertyMapper(titleList, "OR"));
+        
+        /*
+         * This is a 1->N Relationship from QF to index fields
+         */
+                
         ArrayList <String> titleAbstractList = new ArrayList <String> ();
         titleAbstractList.add(IndexConstants.REF_TITLE_ABSTRACT_STEMMED);
         titleAbstractList.add(IndexConstants.REF_TITLE_ABSTRACT_UNSTEMMED);
         
-        propertyMap.put(SearchConstants.REF_TEXT_TITLE_ABSTRACT, new SolrReferenceTextSearchPropertyMapper(titleAbstractList, "OR"));
+        // The 1->N Mapping should be joined with OR's
+        propertyMap.put(SearchConstants.REF_TEXT_TITLE_ABSTRACT, 
+        		new SolrReferenceTextSearchPropertyMapper(titleAbstractList, "OR"));
                 
-        propertyMap.put(FacetConstants.REF_AUTHORS, new SolrPropertyMapper(IndexConstants.REF_AUTHOR_FACET));
-        propertyMap.put(FacetConstants.REF_JOURNALS, new SolrPropertyMapper(IndexConstants.REF_JOURNAL_FACET));
-        propertyMap.put(FacetConstants.REF_YEAR, new SolrPropertyMapper(IndexConstants.REF_YEAR));
-        propertyMap.put(FacetConstants.REF_CURATED_DATA, new SolrPropertyMapper(IndexConstants.REF_HAS_DATA));
+        propertyMap.put(FacetConstants.REF_AUTHORS, 
+        		new SolrPropertyMapper(IndexConstants.REF_AUTHOR_FACET));
+        propertyMap.put(FacetConstants.REF_JOURNALS, 
+        		new SolrPropertyMapper(IndexConstants.REF_JOURNAL_FACET));
+        propertyMap.put(FacetConstants.REF_YEAR, 
+        		new SolrPropertyMapper(IndexConstants.REF_YEAR));
+        propertyMap.put(FacetConstants.REF_CURATED_DATA, 
+        		new SolrPropertyMapper(IndexConstants.REF_HAS_DATA));
         
-        /*
+        // End property mapping section
+        
+        /**
          * What fields might we want highlighted results for?
+         * Any field listed here will be requested for highlighting
+         * all the way down to Solr.
          */
         
         highlightFields.add(IndexConstants.REF_ABSTRACT_STEMMED);
@@ -90,36 +141,67 @@ public class SolrReferenceSummaryBaseHunter extends SolrHunter {
         highlightFields.add(IndexConstants.REF_AUTHOR_FORMATTED);
         
         /* A reverse Mapping of Highlightable fields in the index to what 
-         * parameter it came from
+         * parameter it came from.  This supports the N->1 Mapping 
+         * back to the QF Parameters.
          */
         
-        fieldToParamMap.put(IndexConstants.REF_ABSTRACT_STEMMED, SearchConstants.REF_TEXT_ABSTRACT);
-        fieldToParamMap.put(IndexConstants.REF_ABSTRACT_UNSTEMMED, SearchConstants.REF_TEXT_ABSTRACT);
-        fieldToParamMap.put(IndexConstants.REF_TITLE_STEMMED, SearchConstants.REF_TEXT_TITLE);
-        fieldToParamMap.put(IndexConstants.REF_TITLE_UNSTEMMED, SearchConstants.REF_TEXT_TITLE);
-        fieldToParamMap.put(IndexConstants.REF_TITLE_ABSTRACT_STEMMED, SearchConstants.REF_TEXT_TITLE_ABSTRACT);
-        fieldToParamMap.put(IndexConstants.REF_TITLE_ABSTRACT_UNSTEMMED, SearchConstants.REF_TEXT_TITLE_ABSTRACT);
-        fieldToParamMap.put(IndexConstants.REF_AUTHOR_FORMATTED, SearchConstants.REF_AUTHOR);
-        fieldToParamMap.put(IndexConstants.REF_FIRST_AUTHOR, SearchConstants.REF_AUTHOR);
-        fieldToParamMap.put(IndexConstants.REF_LAST_AUTHOR, SearchConstants.REF_AUTHOR);
+        fieldToParamMap.put(IndexConstants.REF_ABSTRACT_STEMMED,
+        		SearchConstants.REF_TEXT_ABSTRACT);
+        fieldToParamMap.put(IndexConstants.REF_ABSTRACT_UNSTEMMED,
+        		SearchConstants.REF_TEXT_ABSTRACT);
+        fieldToParamMap.put(IndexConstants.REF_TITLE_STEMMED,
+        		SearchConstants.REF_TEXT_TITLE);
+        fieldToParamMap.put(IndexConstants.REF_TITLE_UNSTEMMED,
+        		SearchConstants.REF_TEXT_TITLE);
+        fieldToParamMap.put(IndexConstants.REF_TITLE_ABSTRACT_STEMMED,
+        		SearchConstants.REF_TEXT_TITLE_ABSTRACT);
+        fieldToParamMap.put(IndexConstants.REF_TITLE_ABSTRACT_UNSTEMMED,
+        		SearchConstants.REF_TEXT_TITLE_ABSTRACT);
+        fieldToParamMap.put(IndexConstants.REF_AUTHOR_FORMATTED, 
+        		SearchConstants.REF_AUTHOR);
+        fieldToParamMap.put(IndexConstants.REF_FIRST_AUTHOR, 
+        		SearchConstants.REF_AUTHOR);
+        fieldToParamMap.put(IndexConstants.REF_LAST_AUTHOR, 
+        		SearchConstants.REF_AUTHOR);
         
         
         /*
-         * The base hunter sets nothing at all to be returned.  All of the setting happens in the 
-         * subclasses.
+         * The base hunter sets nothing at all to be returned.  
+         * The specific return requirements are handled in the subclasses.
          */
         
     }
 		
+    /**
+     * checkFilter
+     * @param filter
+     * This private method is unique to the reference grouping of hunters.
+     * Its responsible to create a new query field if both title and abstract
+     * searches are present.
+     * This DEPENDS on the fact that the abstract and title fields are going to 
+     * be found in a nested filter.  If that ever changes this code will break.
+     */
+    
 	private void checkFilter (Filter filter) {
+		
+		// This is our base case, if we find a base filter return.
+		
 	    if (filter.isBasicFilter()) {
 	        return;
 	    }
+	    
+	    // Otherwise recurse, searching for the text searching clauses
+	    
 	    else {
 	        List <Filter> flist = filter.getNestedFilters();
 	        Boolean foundTitle = Boolean.FALSE;
 	        Boolean foundAbstract = Boolean.FALSE;
 	        String textToSearch = "";
+	        
+	        /**
+	         * Iterate through the nested filters.  The two text filters that we
+	         * are interested in should be at the same level.
+	         */
 	        
 	        for (Filter f: flist) {
 	            if (f.isBasicFilter()) {
@@ -133,10 +215,13 @@ public class SolrReferenceSummaryBaseHunter extends SolrHunter {
                     }	                
 	                
 	            }
+	            // Found another nested filter set, recurse!
 	            else {
 	                checkFilter(f);
 	            }
 	        }
+	        
+	        // Found what we are looking for, make a new filter and replace.
 	        
 	        if (foundTitle && foundAbstract) {
 	            filter.setProperty(SearchConstants.REF_TEXT_TITLE_ABSTRACT);
@@ -147,6 +232,16 @@ public class SolrReferenceSummaryBaseHunter extends SolrHunter {
 	    }
 	}
 		
+	/**
+	 * References have implemented the preProcessSearchParams hook.
+	 * For the case of references we need to recursively dig through the 
+	 * search parameters to find the text searching clauses.
+	 * 
+	 * If it finds both an abstract and a title search it replaces those
+	 * with a search to the aggregate field in the index.
+	 * 
+	 */
+	
 	protected SearchParams preProcessSearchParams(SearchParams searchParams) {
 	    
 	    Filter filter = searchParams.getFilter();
