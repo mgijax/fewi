b20ef94c0a5138589c861cad49115d3fc010fe96
Matt Hall <Matt.Hall@jax.org>
2011-05-06 13:21:44 +0000

    Many updates, and removed all of the functions that were not being called from the template.

--- src/org/jax/mgi/fewi/finder/MarkerTissueCountFinder.java
+++ src/org/jax/mgi/fewi/finder/MarkerTissueCountFinder.java
@@ -1,27 +1,15 @@
 package org.jax.mgi.fewi.finder;
 
-import java.util.*;
+import java.util.List;
 
-/*-------------------------------*/
-/* to be changed for each Finder */
-/*-------------------------------*/
-
-import mgi.frontend.datamodel.Marker;
 import mgi.frontend.datamodel.MarkerTissueCount;
 
 import org.jax.mgi.fewi.hunter.FooKeyHunter;
 import org.jax.mgi.fewi.hunter.FooSummaryHunter;
-
-/*----------------------------------------*/
-/* standard classes, used for all Finders */
-/*----------------------------------------*/
-
-// fewi
+import org.jax.mgi.fewi.hunter.SolrMarkerTissueHunter;
+import org.jax.mgi.fewi.objectGatherer.HibernateObjectGatherer;
 import org.jax.mgi.fewi.searchUtil.SearchParams;
 import org.jax.mgi.fewi.searchUtil.SearchResults;
-import org.jax.mgi.fewi.objectGatherer.HibernateObjectGatherer;
-
-// external libs
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,78 +38,36 @@ public class MarkerTissueCountFinder {
 
     @Autowired
     private FooSummaryHunter fooSummaryHunter;
+    
+    @Autowired
+    private SolrMarkerTissueHunter tissueHunter;
 
     @Autowired
-    private HibernateObjectGatherer<MarkerTissueCount> fooGatherer;
+    private HibernateObjectGatherer<MarkerTissueCount> tissueGatherer;
 
+    /*---------------------------------*/
+    /* Retrieval of multiple tissues
+    /*---------------------------------*/
 
-    /*-----------------------------------------*/
-    /* Retrieval of a foo, for a given ID
-    /*-----------------------------------------*/
+    public SearchResults<MarkerTissueCount> getTissues(SearchParams searchParams) {
 
-    public SearchResults<MarkerTissueCount> getFooByID(SearchParams searchParams) {
-
-        logger.debug("->getFooByID()");
+        logger.debug("->getTissues");
 
         // result object to be returned
         SearchResults<MarkerTissueCount> searchResults = new SearchResults<MarkerTissueCount>();
 
+        logger.debug(searchParams.getFilter().toString());
+        
         // ask the hunter to identify which objects to return
-        fooKeyHunter.hunt(searchParams, searchResults);
+        tissueHunter.hunt(searchParams, searchResults);
         logger.debug("->hunter found these resultKeys - "
           + searchResults.getResultKeys());
 
         // gather objects identified by the hunter, add them to the results
-        fooGatherer.setType(MarkerTissueCount.class);
-        List<MarkerTissueCount> fooList
-          = fooGatherer.get( searchResults.getResultKeys() );
-        searchResults.setResultObjects(fooList);
-
-        return searchResults;
-    }
-
-
-	/*--------------------------------------------*/
-	/* Retrieval of a foo, for a given db key
-	/*--------------------------------------------*/
-
-    public SearchResults<MarkerTissueCount> getFooByKey(String dbKey) {
-
-        logger.debug("->getFooByKey()");
-
-        // result object to be returned
-        SearchResults<MarkerTissueCount> searchResults = new SearchResults<MarkerTissueCount>();
-
-        // gather objects, add them to the results
-        fooGatherer.setType(MarkerTissueCount.class);
-        MarkerTissueCount foo = fooGatherer.get( dbKey );
-        searchResults.addResultObjects(foo);
-
-        return searchResults;
-    }
-
-
-    /*---------------------------------*/
-    /* Retrieval of multiple foos
-    /*---------------------------------*/
-
-    public SearchResults<MarkerTissueCount> getFoos(SearchParams searchParams) {
-
-        logger.debug("->getFoos");
-
-        // result object to be returned
-        SearchResults<MarkerTissueCount> searchResults = new SearchResults<MarkerTissueCount>();
-
-        // ask the hunter to identify which objects to return
-        fooSummaryHunter.hunt(searchParams, searchResults);
-        logger.debug("->hunter found these resultKeys - "
-          + searchResults.getResultKeys());
-
-        // gather objects identified by the hunter, add them to the results
-        fooGatherer.setType(MarkerTissueCount.class);
-        List<MarkerTissueCount> fooList
-          = fooGatherer.get( searchResults.getResultKeys() );
-        searchResults.setResultObjects(fooList);
+        tissueGatherer.setType(MarkerTissueCount.class);
+        List<MarkerTissueCount> tissueList
+          = tissueGatherer.get( searchResults.getResultKeys() );
+        searchResults.setResultObjects(tissueList);
 
         return searchResults;
     }
