cbff714df4b99a5a983f49cbbb111e0e0b0da5d7
Matt Hall <Matt.Hall@jax.org>
2011-05-23 17:44:10 +0000

    Initial version of this report

---
+++ src/org/jax/mgi/fewi/view/TextTissueSummary.java
@@ -0,0 +1,43 @@
+package org.jax.mgi.fewi.view;
+
+import java.io.BufferedWriter;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jax.mgi.fewi.controller.MarkerTissueCountController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import mgi.frontend.datamodel.MarkerTissueCount;
+
+public class TextTissueSummary extends AbstractTextView {
+
+    private Logger logger
+    = LoggerFactory.getLogger(this.getClass().getName());
+
+    
+	@Override
+	protected void buildTextDocument(Map<String, Object> model, BufferedWriter writer,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+	    
+		response.setHeader("Content-Disposition","attachment; filename=\"tissueReport.txt\"");
+		System.out.println(response.getCharacterEncoding());
+		
+		List<MarkerTissueCount> tissues = (List<MarkerTissueCount>) model.get("results");
+		
+		for (MarkerTissueCount mtc : tissues) {
+			
+			writer.write("TS" + mtc.getStructure() + "\t");
+			writer.write(mtc.getAllResultCount() + "\t");
+			writer.write(mtc.getDetectedResultCount() + "\t");
+			writer.write(mtc.getNotDetectedResultCount() + "\r\n");
+			//writer.newLine();
+		}
+
+	}
+
+}
