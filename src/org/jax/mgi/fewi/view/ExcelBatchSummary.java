43759a3a4164fb1ba402a55e71893f9f52b85287
Mark Airey <Mark.Airey@jax.org>
2011-06-15 18:53:50 +0000

    adding batch query downloadable reports

--- src/org/jax/mgi/fewi/view/ExcelBatchSummary.java
+++ src/org/jax/mgi/fewi/view/ExcelBatchSummary.java
@@ -1,17 +1,27 @@
 package org.jax.mgi.fewi.view;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mgi.frontend.datamodel.Annotation;
+import mgi.frontend.datamodel.BatchMarkerAllele;
+import mgi.frontend.datamodel.BatchMarkerSnp;
 import mgi.frontend.datamodel.BatchMarkerId;
 import mgi.frontend.datamodel.Marker;
+import mgi.frontend.datamodel.MarkerID;
+import mgi.frontend.datamodel.MarkerLocation;
+import mgi.frontend.datamodel.MarkerTissueCount;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.jax.mgi.fewi.forms.BatchQueryForm;
+import org.jax.mgi.fewi.util.DBConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.view.document.AbstractExcelView;
@@ -27,26 +37,342 @@ public class ExcelBatchSummary extends AbstractExcelView {
 			throws Exception {
 		
 		logger.debug("buildExcelDocument");
+		BatchQueryForm queryForm = (BatchQueryForm)model.get("queryForm");
 		List<BatchMarkerId> results = (List<BatchMarkerId>) model.get("results");
 
-		HSSFSheet sheet = workbook.createSheet();
+		HSSFSheet sheet = workbook.createSheet();		
+		genHeader(sheet.createRow(0), queryForm);
 		
-		HSSFRow header = sheet.createRow(0);
-		header.createCell(0).setCellValue("Citation");
-		
-		HSSFRow row;
+		HSSFRow row, addlRow;
 		int rownum = 1;
+		int col;
 		
 		Marker m;
+		List<MarkerID> ids;
+		List<List<List<String>>> associations = new ArrayList<List<List<String>>>();
+
 		for (BatchMarkerId id : results) {
-			row = sheet.createRow(rownum++);
+			associations = new ArrayList<List<List<String>>>();
+
 			
-			m = id.getMarker();
-			if (m != null){ 
-				row.createCell(0).setCellValue(m.getSymbol());
-			} else {
-				row.createCell(0).setCellValue(id.getTerm());
+			row = sheet.createRow(rownum++);
+			col = 0;
+			logger.debug("input: " + id.getTerm());
+			row.createCell(col++).setCellValue(id.getTerm());
+			logger.debug("type: " + id.getTermType());
+			row.createCell(col++).setCellValue(id.getTermType());
+			
+			m = id.getMarker();			
+			
+			if (m != null){
+				
+				ids = m.getIds();
+				
+				row.createCell(col++).setCellValue(m.getPrimaryID());
+
+				if(queryForm.getNomenclature()){
+					logger.debug("nomen");
+					row.createCell(col++).setCellValue(m.getSymbol());
+					row.createCell(col++).setCellValue(m.getName());
+					row.createCell(col++).setCellValue(m.getMarkerType());
+				}
+				if(queryForm.getLocation()){
+					logger.debug("loc");
+					MarkerLocation loc = m.getPreferredCoordinates();
+					if (loc != null) {
+						row.createCell(col++).setCellValue(loc.getChromosome());
+						row.createCell(col++).setCellValue(loc.getStrand());
+						row.createCell(col++).setCellValue(loc.getStartCoordinate());
+						row.createCell(col++).setCellValue(loc.getEndCoordinate());
+					} else {
+						row.createCell(col++).setCellValue("UN");
+						col += 3;
+					}
+				}
+				
+				// build associations matrix
+				if(queryForm.getEnsembl()){
+					logger.debug("ensembl");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> eIds;
+	        		if(ids != null){
+	        			for (MarkerID mId : ids) {
+	    					if (mId.getLogicalDB().equals(DBConstants.PROVIDER_ENSEMBL)){
+	    						eIds = new ArrayList<String>();
+	    						eIds.add(mId.getAccID());
+	    						wrapper.add(eIds);
+	    					}						
+	    				}
+	        		}
+	        		associations.add(wrapper);
+				}
+				if(queryForm.getEntrez()){
+					logger.debug("entrez");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> eIds;
+		    		if(ids != null){
+		    			for (MarkerID mId : ids) {
+							if (mId.getLogicalDB().equals(DBConstants.PROVIDER_ENTREZGENE)){
+								eIds = new ArrayList<String>();
+								eIds.add(mId.getAccID());
+								wrapper.add(eIds);
+							}						
+						}
+		    		}
+		    		associations.add(wrapper);
+				}
+				if(queryForm.getVega()){
+					logger.debug("vega");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> vIds;
+		    		if(ids != null){
+		    			for (MarkerID mId : ids) {
+							if (mId.getLogicalDB().equals(DBConstants.PROVIDER_VEGA)){
+								vIds = new ArrayList<String>();
+								vIds.add(mId.getAccID());
+								wrapper.add(vIds);
+							}						
+						}
+		    		}
+		    		associations.add(wrapper);
+				}
+				
+				if(queryForm.getGo()){
+					logger.debug("go");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> goIds;
+	    			for (Annotation goAnnot : m.getGoAnnotations()) {
+	    				goIds = new ArrayList<String>();
+	    				goIds.add(goAnnot.getTermID());
+	    				goIds.add(goAnnot.getTerm());
+	    				goIds.add(goAnnot.getEvidenceCode());
+	    				wrapper.add(goIds);
+					}		    		
+		    		associations.add(wrapper);
+				} else if(queryForm.getMp()){
+					logger.debug("mp");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> mpIds;
+	    			for (Annotation mpAnnot : m.getMPAnnotations()) {
+	    				mpIds = new ArrayList<String>();
+	    				mpIds.add(mpAnnot.getTermID());
+	    				mpIds.add(mpAnnot.getTerm());
+						wrapper.add(mpIds);
+					}		    		
+		    		associations.add(wrapper);
+				} else if(queryForm.getOmim()){
+					logger.debug("omim");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> mpIds;
+	    			for (Annotation omimAnnot : m.getOMIMAnnotations()) {
+	    				mpIds = new ArrayList<String>();
+	    				mpIds.add(omimAnnot.getTermID());
+	    				mpIds.add(omimAnnot.getTerm());
+	    				wrapper.add(mpIds);
+					}		
+		    		associations.add(wrapper);
+				} else if(queryForm.getAllele()){
+					logger.debug("allele");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> alleles;
+	    			for (BatchMarkerAllele bma : m.getBatchMarkerAlleles()) {
+	    				alleles = new ArrayList<String>();
+	    				alleles.add(bma.getAlleleID());
+	    				alleles.add(bma.getAlleleSymbol());
+	    				wrapper.add(alleles);
+					}
+		    		associations.add(wrapper);
+				} else if(queryForm.getExp()){
+					logger.debug("exp");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> expression;
+	    			for (MarkerTissueCount tissue : m.getMarkerTissueCounts()) {
+	    				expression = new ArrayList<String>();
+	    				expression.add(tissue.getStructure());
+	    				expression.add(String.valueOf(tissue.getAllResultCount()));
+	    				expression.add(String.valueOf(tissue.getDetectedResultCount()));
+	    				expression.add(String.valueOf(tissue.getNotDetectedResultCount()));
+	    				wrapper.add(expression);
+					}
+		    		associations.add(wrapper);
+				} else if(queryForm.getRefsnp()){
+					logger.debug("refsnp");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> refSnpIds;
+        			for (BatchMarkerSnp snp : m.getBatchMarkerSnps()) {
+        				refSnpIds = new ArrayList<String>();
+        				refSnpIds.add(snp.getSnpID());
+        				wrapper.add(refSnpIds);
+    				}
+		    		associations.add(wrapper);
+				} else if(queryForm.getRefseq()){
+					logger.debug("refseq");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> refSeqIds;
+	        		if(ids != null){
+	        			for (MarkerID mId : ids) {
+	    					if (mId.getLogicalDB().equals(DBConstants.PROVIDER_REFSEQ) ||
+	    							mId.getLogicalDB().equals("Sequence DB")){
+	    						refSeqIds = new ArrayList<String>();
+	    						refSeqIds.add(mId.getAccID());
+	    						wrapper.add(refSeqIds);
+	    					}						
+	    				}
+	        		}
+		    		associations.add(wrapper);
+				} else if(queryForm.getUniprot()){
+					logger.debug("uniprot");
+					List<List<String>> wrapper = new ArrayList<List<String>>();
+					List<String> uniProtIds;
+	        		if(ids != null){
+	        			for (MarkerID mId : ids) {
+	    					if (mId.getLogicalDB().equals(DBConstants.PROVIDER_SWISSPROT) ||
+	    							mId.getLogicalDB().equals(DBConstants.PROVIDER_TREMBL)){
+	    						uniProtIds = new ArrayList<String>();	    						
+	    						uniProtIds.add(mId.getAccID());
+	    						wrapper.add(uniProtIds);
+	    					}						
+	    				}
+	        		}	    		
+		    		associations.add(wrapper);
+				}
+				
+				logger.debug("assoc");
+				for (List<List<String>> assoc: associations) {
+					if (assoc.size() == 0) {
+						List<String> empty = new ArrayList<String>();
+						empty.add("");
+						assoc.add(empty);
+					}
+				}
+				logger.debug("combine");
+				List<List<String>> combineResults = new ArrayList<List<String>>();
+				if (associations.size() > 0) {
+					List<String> l;
+					for (List<String> list: associations.get(0)) {
+						l = new ArrayList<String>();
+						for (String s: list) {
+							l.add(s);
+						}
+						combineResults.add(l);
+					}				
+					if (associations.size() > 1) {				
+						for (int i = 1; i < associations.size(); i++) {
+							combineResults = combineSets(combineResults, associations.get(i));
+						}				
+					}
+				}
+				logger.debug("combineResults: " + combineResults.size());
+				
+				int curRow = rownum - 1;
+				if (combineResults.size() > 0){
+					boolean newRow = false;
+					int storeCol = col;
+					
+					for (List<String> ls: combineResults) {
+						logger.debug("combineCols: " + ls.size());
+						if (newRow) {
+							addlRow = sheet.createRow(rownum++);
+							for (int i = 0; i < col; i++) {
+								logger.debug("copy cell: " + i);
+								if (row.getCell(i).getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
+									addlRow.createCell(i).setCellValue(row.getCell(i).getNumericCellValue());
+								} else if (row.getCell(i).getCellType() == HSSFCell.CELL_TYPE_STRING){
+									addlRow.createCell(i).setCellValue(row.getCell(i).getStringCellValue());
+								}
+								
+								
+							}
+							row = addlRow;
+						}
+						for (String s: ls) {
+							row.createCell(col++).setCellValue(s);
+						}
+						
+						newRow = true;
+						col = storeCol;
+					} 
+					workbook.setRepeatingRowsAndColumns(0,0,0,curRow,curRow + combineResults.size());
+				}
 			}
 		}
+		for (int i = 0; i < 20; i++) {
+			sheet.autoSizeColumn(i);
+			
+		}
+	}
+	
+	private  HSSFRow genHeader(HSSFRow headerRow, BatchQueryForm queryForm){
+		int i = 0;
+		headerRow.createCell(i++).setCellValue("Input");
+		headerRow.createCell(i++).setCellValue("Input Type");
+		headerRow.createCell(i++).setCellValue("MGI Gene/Marker ID");
+		 
+		if(queryForm.getNomenclature()){
+			headerRow.createCell(i++).setCellValue("Symbol");
+			headerRow.createCell(i++).setCellValue("Name");
+			headerRow.createCell(i++).setCellValue("Feature Type");
+		}
+		if(queryForm.getLocation()){
+			headerRow.createCell(i++).setCellValue("Chr");
+			headerRow.createCell(i++).setCellValue("Strand");
+			headerRow.createCell(i++).setCellValue("Start");
+			headerRow.createCell(i++).setCellValue("End");
+		}
+		if(queryForm.getEnsembl()){
+			headerRow.createCell(i++).setCellValue("Ensembl IDs");
+		}
+		if(queryForm.getEntrez()){
+			headerRow.createCell(i++).setCellValue("Entrez Gene IDs");
+		}
+		if(queryForm.getVega()){
+			headerRow.createCell(i++).setCellValue("Vega IDs");
+		}
+		
+		if(queryForm.getGo()){
+			headerRow.createCell(i++).setCellValue("GO ID");
+			headerRow.createCell(i++).setCellValue("Term");
+			headerRow.createCell(i++).setCellValue("Code");
+		} else if(queryForm.getMp()){
+			headerRow.createCell(i++).setCellValue("MP ID");
+			headerRow.createCell(i++).setCellValue("Term");
+		} else if(queryForm.getOmim()){
+			headerRow.createCell(i++).setCellValue("OMIM ID");
+			headerRow.createCell(i++).setCellValue("Term");
+		} else if(queryForm.getAllele()){
+			headerRow.createCell(i++).setCellValue("Allele ID");
+			headerRow.createCell(i++).setCellValue("Symbol");
+		} else if(queryForm.getExp()){
+			headerRow.createCell(i++).setCellValue("Anatomical Structure");
+			headerRow.createCell(i++).setCellValue("Assay Results");
+			headerRow.createCell(i++).setCellValue("Detected");
+			headerRow.createCell(i++).setCellValue("Not Detected");
+		} else if(queryForm.getRefsnp()){
+			headerRow.createCell(i++).setCellValue("RefSNP IDs");
+		} else if(queryForm.getRefseq()){
+			headerRow.createCell(i++).setCellValue("GenBank/RefSeq IDs");
+		} else if(queryForm.getUniprot()){
+			headerRow.createCell(i++).setCellValue("Uniprot IDs");
+		}
+		return headerRow;
+	}
+	
+	private List<List<String>> combineSets (List<List<String>> results, List<List<String>> add){
+		List<List<String>> newResults = new ArrayList<List<String>>();
+		List<String> newList;
+		for (List<String> a: results ){
+			for (List<String> list: add) {
+				newList = new ArrayList<String>();
+				newList.addAll(a);
+				logger.debug("pre: " + newList.size());
+				for (String s: list) {
+					logger.debug(s);
+					newList.add(s);
+				}
+				logger.debug("post: " + newList.size());
+				newResults.add(newList);
+			}
+		}
+		return newResults;
 	}
 }
--- src/org/jax/mgi/fewi/view/TextBatchSummary.java
+++ src/org/jax/mgi/fewi/view/TextBatchSummary.java
@@ -1,16 +1,30 @@
 package org.jax.mgi.fewi.view;
 
 import java.io.BufferedWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mgi.frontend.datamodel.Annotation;
+import mgi.frontend.datamodel.BatchMarkerAllele;
 import mgi.frontend.datamodel.BatchMarkerId;
+import mgi.frontend.datamodel.BatchMarkerSnp;
 import mgi.frontend.datamodel.Marker;
+import mgi.frontend.datamodel.MarkerID;
+import mgi.frontend.datamodel.MarkerLocation;
+import mgi.frontend.datamodel.MarkerTissueCount;
+
+import org.jax.mgi.fewi.forms.BatchQueryForm;
+import org.jax.mgi.fewi.util.DBConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TextBatchSummary extends AbstractTextView {
+	
+	private Logger logger = LoggerFactory.getLogger(TextBatchSummary.class);
 
 	@Override
 	protected void buildTextDocument(Map<String, Object> model, BufferedWriter writer,
@@ -20,21 +34,218 @@ public class TextBatchSummary extends AbstractTextView {
 		response.setHeader("Content-Disposition","attachment; filename=\"batchReport.txt\"");
 		System.out.println(response.getCharacterEncoding());
 		
+		BatchQueryForm queryForm = (BatchQueryForm)model.get("queryForm");
 		List<BatchMarkerId> results = (List<BatchMarkerId>) model.get("results");
 		
 		Marker m;
+		List<MarkerID> ids;
+		StringBuffer markerInfo;
+		List<List<String>> associations;
+		
 		for (BatchMarkerId id : results) {
+			markerInfo = new StringBuffer();
+			associations = new ArrayList<List<String>>();
 			m = id.getMarker();
+			
+			
+
+			
+			markerInfo.append(id.getTerm() + "\t");
+			markerInfo.append(id.getTermType() + "\t");
+			
 			if (m != null){
-				writer.write(m.getSymbol()); 
-			} else {
-				writer.write(id.getTerm()); 
+				ids = m.getIds();
+				markerInfo.append(m.getPrimaryID()); 
+		 
+				if(queryForm.getNomenclature()){
+					markerInfo.append("\t");
+					markerInfo.append(m.getSymbol() + "\t");
+					markerInfo.append(m.getName() + "\t");
+					markerInfo.append(m.getMarkerType());
+				}
+				if(queryForm.getLocation()){
+					markerInfo.append("\t");
+					MarkerLocation loc = m.getPreferredCoordinates();
+					if (loc != null) {
+						markerInfo.append(loc.getChromosome() + "\t");
+						markerInfo.append(loc.getStrand() + "\t");
+						markerInfo.append(String.format("%.0f", loc.getStartCoordinate()) + "\t");
+						markerInfo.append(String.format("%.0f", loc.getEndCoordinate()));
+					} else {
+						markerInfo.append("UN\t\t");
+					}
+				}
+				
+				// build associations matrix
+				if(queryForm.getEnsembl()){
+					logger.debug("ensembl");
+					List<String> eIds = new ArrayList<String>();
+	        		if(ids != null){
+	        			for (MarkerID mId : ids) {
+	    					if (mId.getLogicalDB().equals(DBConstants.PROVIDER_ENSEMBL)){
+	    						eIds.add(mId.getAccID());
+	    					}						
+	    				}
+	        		}
+	        		associations.add(eIds);
+				}
+				if(queryForm.getEntrez()){
+					logger.debug("entrez");
+					List<String> eIds = new ArrayList<String>();
+		    		if(ids != null){
+		    			for (MarkerID mId : ids) {
+							if (mId.getLogicalDB().equals(DBConstants.PROVIDER_ENTREZGENE)){
+								eIds.add(mId.getAccID());
+							}						
+						}
+		    		}
+		    		associations.add(eIds);
+				}
+				if(queryForm.getVega()){
+					logger.debug("vega");
+					List<String> vIds = new ArrayList<String>();
+		    		if(ids != null){
+		    			for (MarkerID mId : ids) {
+							if (mId.getLogicalDB().equals(DBConstants.PROVIDER_VEGA)){
+								vIds.add(mId.getAccID());
+							}						
+						}
+		    		}
+		    		associations.add(vIds);
+				}
+				
+				if(queryForm.getGo()){
+					logger.debug("go");
+					List<String> goIds = new ArrayList<String>();
+					StringBuffer go;
+	    			for (Annotation goAnnot : m.getGoAnnotations()) {
+	    				go = new StringBuffer();
+						go.append(goAnnot.getTermID() + "\t");
+						go.append(goAnnot.getTerm() + "\t");
+						go.append(goAnnot.getEvidenceCode());
+						goIds.add(go.toString());
+					}
+		    		associations.add(goIds);
+				} else if(queryForm.getMp()){
+					logger.debug("mp");
+					List<String> mpIds = new ArrayList<String>();
+					StringBuffer mp;
+	    			for (Annotation mpAnnot : m.getMPAnnotations()) {
+	    				mp = new StringBuffer();
+						mp.append(mpAnnot.getTermID() + "\t");
+						mp.append(mpAnnot.getTerm() + "\t");
+						mp.append(mpAnnot.getEvidenceCode());
+						mpIds.add(mp.toString());
+					}
+		    		associations.add(mpIds);
+				} else if(queryForm.getOmim()){
+					logger.debug("omim");
+					List<String> mpIds = new ArrayList<String>();
+					StringBuffer mp;
+	    			for (Annotation mpAnnot : m.getMPAnnotations()) {
+	    				mp = new StringBuffer();
+						mp.append(mpAnnot.getTermID() + "\t");
+						mp.append(mpAnnot.getTerm());
+	
+						mpIds.add(mp.toString());
+					}
+		    		associations.add(mpIds);
+				} else if(queryForm.getAllele()){
+					logger.debug("allele");
+					List<String> alleles = new ArrayList<String>();
+					StringBuffer allele;
+	    			for (BatchMarkerAllele bma : m.getBatchMarkerAlleles()) {
+	    				allele = new StringBuffer();
+	    				allele.append(bma.getAlleleID() + "\t");
+	    				allele.append(bma.getAlleleSymbol());
+	    				alleles.add(allele.toString());
+					}
+		    		associations.add(alleles);
+				} else if(queryForm.getExp()){
+					logger.debug("exp");
+					List<String> expression = new ArrayList<String>();
+					StringBuffer exp;
+	    			for (MarkerTissueCount tissue : m.getMarkerTissueCounts()) {
+	    				exp = new StringBuffer();
+	    				exp.append(tissue.getStructure() + "\t");
+	    				exp.append(tissue.getAllResultCount() + "\t");
+	    				exp.append(tissue.getDetectedResultCount() + "\t");
+	    				exp.append(tissue.getNotDetectedResultCount());
+						expression.add(exp.toString());
+					}
+		    		associations.add(expression);
+				} else if(queryForm.getRefsnp()){
+					logger.debug("refsnp");
+					List<String> refSnpIds = new ArrayList<String>();					
+        			for (BatchMarkerSnp snp : m.getBatchMarkerSnps()) {
+        				refSnpIds.add(snp.getSnpID());
+    				}
+		    		associations.add(refSnpIds);
+				} else if(queryForm.getRefseq()){
+					logger.debug("refseq");
+					List<String> refSeqIds = new ArrayList<String>();
+	        		if(ids != null){
+	        			for (MarkerID mId : ids) {
+	    					if (mId.getLogicalDB().equals(DBConstants.PROVIDER_REFSEQ) ||
+	    							mId.getLogicalDB().equals("Sequence DB")){
+	    						refSeqIds.add(mId.getAccID());
+	    					}						
+	    				}
+	        		}
+		    		associations.add(refSeqIds);
+				} else if(queryForm.getUniprot()){
+					logger.debug("uniprot");
+					List<String> uniProtIds = new ArrayList<String>();
+	        		if(ids != null){
+	        			for (MarkerID mId : ids) {
+	    					if (mId.getLogicalDB().equals(DBConstants.PROVIDER_SWISSPROT) ||
+	    							mId.getLogicalDB().equals(DBConstants.PROVIDER_TREMBL)){
+	    						uniProtIds.add(mId.getAccID());
+	    					}						
+	    				}
+	        		}
+		    		associations.add(uniProtIds);
+				}
+				
+				for (List<String> assoc: associations) {
+					if (assoc.size() == 0) {
+						assoc.add("");
+					}
+				}
+				
+				List<String> combineResults = new ArrayList<String>();
+				if (associations.size() > 0) {
+					combineResults = associations.get(0);
+					
+					if (associations.size() > 1) {				
+						for (int i = 1; i < associations.size(); i++) {
+							combineResults = combineSets(combineResults, associations.get(i));
+						}				
+					}
+				}
+				if (combineResults.size() > 0){
+					for (String s: combineResults) {
+						logger.debug(s + ".");
+						writer.write(markerInfo.toString());
+						writer.write("\t" + s);		
+						writer.write("\r\n");
+					}
+				} else {
+					writer.write(markerInfo.toString());		
+					writer.write("\r\n");
+				}
 			}
-
-			writer.write("\n\r");
-			//writer.newLine();
 		}
-
+	}
+	
+	private List<String> combineSets (List<String> listA, List<String> listB){
+		List<String> results = new ArrayList<String>();
+		for (String a: listA ){
+			for (String b: listB ) {
+				results.add(a + "\t" + b);
+			}
+		}
+		return results;
 	}
 
 }
