eed1de4d3642513ecb54584305e0973ae2f0338a
Jonathan Beal <Jon.Beal@jax.org>
2011-08-12 21:23:49 +0000

    added journalSortLower

--- src/org/jax/mgi/fewi/searchUtil/SortConstants.java
+++ src/org/jax/mgi/fewi/searchUtil/SortConstants.java
@@ -7,6 +7,7 @@ public class SortConstants {
 	public static final String REF_JOURNAL         = "journal";
 	public static final String REF_AUTHORS         = "authors";
 	public static final String REF_BY_AUTHOR       = "authorSort";
+	public static final String REF_JOURNAL_AC      = "journalSortLower";
 	public static final String REF_YEAR            = "year";
 
 	// recombinase summary page
