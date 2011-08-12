5cb9926d43d5f2f7b33f2d706640fe84d46a23d9
Jonathan Beal <Jon.Beal@jax.org>
2011-08-12 21:24:51 +0000

    added sorting of journal auto-complete

--- src/org/jax/mgi/fewi/hunter/SolrJournalsACHunter.java
+++ src/org/jax/mgi/fewi/hunter/SolrJournalsACHunter.java
@@ -2,6 +2,8 @@ package org.jax.mgi.fewi.hunter;
 
 import org.jax.mgi.fewi.propertyMapper.SolrPropertyMapper;
 import org.jax.mgi.fewi.searchUtil.SearchConstants;
+import org.jax.mgi.fewi.searchUtil.SortConstants;
+import org.jax.mgi.fewi.sortMapper.SolrSortMapper;
 import org.jax.mgi.shr.fe.IndexConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -24,6 +26,11 @@ public class SolrJournalsACHunter extends SolrHunter {
         propertyMap.put(SearchConstants.REF_JOURNAL, new SolrPropertyMapper(IndexConstants.REF_JOURNAL));
         propertyMap.put(SearchConstants.AC_FOR_GXD, new SolrPropertyMapper(IndexConstants.AC_FOR_GXD));
         
+	/* sort journal auto-complete results in a case-insensitive manner
+	*/
+	sortMap.put(SortConstants.REF_JOURNAL_AC,
+	    new SolrSortMapper(IndexConstants.REF_JOURNAL_SORT_LOWER));
+
         /*
          * For this hunter we are not interested in keys, we instead want to pack 
          * a specific field from the returned solr documents into the response.
