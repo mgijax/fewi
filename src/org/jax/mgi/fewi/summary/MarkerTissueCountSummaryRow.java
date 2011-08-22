4ac0dac95591080fb9924843d50298f8b48b8535
Peter Frost <Pete.Frost@jax.org>
2011-08-22 18:20:51 +0000

    Updated for URL changes

--- src/org/jax/mgi/fewi/summary/MarkerTissueCountSummaryRow.java
+++ src/org/jax/mgi/fewi/summary/MarkerTissueCountSummaryRow.java
@@ -12,64 +12,75 @@ import org.slf4j.LoggerFactory;
  */
 public class MarkerTissueCountSummaryRow {
 
-	//-------------------
-	// instance variables
-	//-------------------
+  //-------------------
+  // instance variables
+  //-------------------
 
-    private Logger logger = LoggerFactory.getLogger(MarkerTissueCountSummaryRow.class);
+  private Logger logger = LoggerFactory.getLogger(MarkerTissueCountSummaryRow.class);
 
-	// encapsulated row object
-	private MarkerTissueCount mtc;
+  // encapsulated row object
+  private MarkerTissueCount mtc;
 
-	// config values
-    String fewiUrl = ContextLoader.getConfigBean().getProperty("FEWI_URL");
+  // config values
+  String fewiUrl   = ContextLoader.getConfigBean().getProperty("FEWI_URL");
+  String pywiUrl   = ContextLoader.getConfigBean().getProperty("WI_URL");
+  String javawiUrl = ContextLoader.getConfigBean().getProperty("JAVAWI_URL");
 
 
-	//-------------
-	// constructors
-	//-------------
+  //-------------
+  // constructors
+  //-------------
 
-	// hide the default constructor - we NEED a marker tissue count to wrap
-    private MarkerTissueCountSummaryRow () {}
+  // hide the default constructor - we NEED a marker tissue count to wrap
+  private MarkerTissueCountSummaryRow () {}
 
-    public MarkerTissueCountSummaryRow (MarkerTissueCount mtc) {
-    	this.mtc = mtc;
-    	return;
+  public MarkerTissueCountSummaryRow (MarkerTissueCount mtc) {
+    this.mtc = mtc;
+    return;
+  }
+
+
+  //------------------------------------------------------------------------
+  // public instance methods;  JSON serializer will call all public methods
+  //------------------------------------------------------------------------
+
+
+  // structure field
+  public String getStructure() {
+    return "<a href=\"" + pywiUrl + "searches/anatdict.cgi?id=" +mtc.getStructureKey() + "\">" + "TS" + mtc.getStructure() + "</a>";
+  }
+
+
+  // count of ALL tissues
+  public String getAll() {
+    if (mtc.getAllResultCount() > 0) {
+      return "<a href=\"" + pywiUrl + "searches/expression_report.cgi?sort=Anatomical structure&returnType=assay results&_Marker_key=" + mtc.getMarkerKey()+ "&_Structure_key=" + mtc.getStructureKey()+ "\">" + mtc.getAllResultCount() + "</a>";
     }
-
-
-    //------------------------------------------------------------------------
-    // public instance methods;  JSON serializer will call all public methods
-    //------------------------------------------------------------------------
-
-    public String getAll() {
-    	if (mtc.getAllResultCount() > 0) {
-    		return "<a href=\"" + fewiUrl + "gxd/assay/?q=markerKey=" + mtc.getMarkerKey()+ "&structureKey=" + mtc.getStructureKey()+ "\">" + mtc.getAllResultCount() + "</a>";
-    	}
-    	else {
-    		return "0";
-    	}
+    else {
+      return "0";
     }
+  }
 
-    public String getNotDetected() {
-    	if (mtc.getNotDetectedResultCount() > 0) {
-    		return "<a href=\"" + fewiUrl + "gxd/assay/?q=markerKey=" + mtc.getMarkerKey()+ "&structureKey=" + mtc.getStructureKey()+ "&detected=0\">" + mtc.getNotDetectedResultCount() + "</a>";
-    	}
-    	else {
-    		return "0";
-    	}
-    }
 
-    public String getDetected() {
-    	if (mtc.getDetectedResultCount() > 0) {
-    		return "<a href=\"" + fewiUrl + "gxd/assay/?q=markerKey=" + mtc.getMarkerKey()+ "&structureKey=" + mtc.getStructureKey()+ "&detected=1\">" + mtc.getDetectedResultCount() + "</a>";
-    	}
-    	else {
-    		return "0";
-    	}
+  // count of tissues which this gene were detected
+  public String getDetected() {
+    if (mtc.getDetectedResultCount() > 0) {
+      return "<a href=\"" + pywiUrl + "searches/expression_report.cgi?sort=Anatomical%20structure&expressed=1&returnType=assay%20results&_Marker_key=" + mtc.getMarkerKey()+ "&_Structure_key=" + mtc.getStructureKey() + "\">" + mtc.getAllResultCount() + "</a>";
     }
-    
-    public String getStructure() {
-    	return "<a href=\"" + fewiUrl + "ad/" +mtc.getStructureKey() + "\">" + "TS" + mtc.getStructure() + "</a>";
+    else {
+      return "0";
     }
+  }
+
+
+  // count of tissues which this gene were NOT detected
+  public String getNotDetected() {
+    if (mtc.getNotDetectedResultCount() > 0) {
+      return "<a href=\"" + pywiUrl + "searches/expression_report.cgi?sort=Anatomical%20structure&expressed=0&returnType=assay%20results&_Marker_key=" + mtc.getMarkerKey()+ "&_Structure_key=" + mtc.getStructureKey() + "\">" + mtc.getAllResultCount() + "</a>";
+    }
+    else {
+      return "0";
+    }
+  }
+
 }
