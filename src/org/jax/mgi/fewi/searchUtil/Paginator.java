63b683522e19eec6bb7567ba7c0471aa9426bbc6
Peter Frost <Pete.Frost@jax.org>
2011-09-12 18:44:38 +0000

    Cleaning defaults and ensuring startIndex is not negative

--- src/org/jax/mgi/fewi/searchUtil/Paginator.java
+++ src/org/jax/mgi/fewi/searchUtil/Paginator.java
@@ -1,22 +1,35 @@
 package org.jax.mgi.fewi.searchUtil;
 
 public class Paginator {
-	
+
 	private Integer startIndex = 0;
-	private Integer results = 25;
-	
+	private Integer results;
+	private Integer resultsDefault = 25;
+
+
+    // startIndex
 	public Integer getStartIndex() {
+        if (startIndex < 0){return 0;}
 		return startIndex;
 	}
 	public void setStartIndex(Integer startIndex) {
 		this.startIndex = startIndex;
 	}
+
+    // results
 	public Integer getResults() {
+        if (results==null){return resultsDefault;}
 		return results;
 	}
 	public void setResults(Integer results) {
 		this.results = results;
 	}
+	public void setResultsDefault(Integer resultsDefault) {
+		this.resultsDefault = resultsDefault;
+	}
+
+
+    // toString for debugging purposes
 	@Override
 	public String toString() {
 		return "Paginator [results=" + results + ", startIndex=" + startIndex
