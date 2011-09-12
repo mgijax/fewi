ad6f3cc191e5ee2e13ff0769e283466dd4ca4c06
Peter Frost <Pete.Frost@jax.org>
2011-09-12 18:41:32 +0000

    Initial version

---
+++ src/org/jax/mgi/fewi/util/PaginationControls.java
@@ -0,0 +1,152 @@
+package org.jax.mgi.fewi.util;
+
+// standard java
+import java.util.*;
+
+// internal
+import org.jax.mgi.fewi.config.ContextLoader;
+import org.jax.mgi.fewi.searchUtil.Paginator;
+
+//external
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+* A PaginationControls object encapsulates the logic for MGI's internal
+* pagination, assisting in the display of pagination droplists and
+* display-time boolean logic.  Wraps Spring-loaded Paginator object.
+*/
+public class PaginationControls
+{
+
+    //---------------------------------------------------------------
+    // instance variables
+    //---------------------------------------------------------------
+
+    // logger for this class
+    private Logger logger
+      = LoggerFactory.getLogger(this.getClass().getName());
+
+	// pagination data
+	private Integer resultsTotal = 0;
+	private Integer startIndex = 0;
+	private Integer results = 0;
+
+
+    //---------------------------------------------------------------
+    // Constructors
+    //---------------------------------------------------------------
+
+    /**
+    * uses spring-filled paginator object to retrieve pagination values
+    */
+    public PaginationControls (Paginator paginator){
+        startIndex  = paginator.getStartIndex();
+        results     = paginator.getResults();
+
+        // ensure start index isn't negative
+        if (startIndex < 0) {startIndex=0;}
+
+        return;
+    }
+
+
+    //---------------------------------------------------------------
+    // Public access methods
+    //---------------------------------------------------------------
+
+	public Integer getStartIndex() {
+		return startIndex;
+	}
+	public Integer getStartIndexDisplay() {
+		return startIndex + 1;
+	}
+	public Integer getResults() {
+		return results;
+	}
+
+    // total # of results
+	public Integer getResultsTotal() {
+		return resultsTotal;
+	}
+	public void setResultsTotal(Integer resultsTotal) {
+		this.resultsTotal = resultsTotal;
+	}
+
+
+    // total being displayed
+	public Integer getLastResultDisplayed() {
+
+        // derive ending
+        Integer remainingResults = resultsTotal - startIndex;
+        if (remainingResults < results) {
+          return this.getResultsTotal();
+	    }
+	    return startIndex + results;
+	}
+
+    // drop list
+	public String getDropList() {
+
+        String rangeDropList = "<select name='results' " +
+            "onchange='this.form.submit();'> " +
+            "<OPTION VALUE='10'>10 " +
+            "<OPTION VALUE='25'>25 " +
+            "<OPTION VALUE='50'>50 " +
+            "<OPTION VALUE='1000'>All " +
+            "</select>";
+        if (results == 25){
+            rangeDropList = "<select name='results' " +
+            "onchange='this.form.submit();'> " +
+            "<OPTION VALUE='10'>10 " +
+            "<OPTION VALUE='25' selected>25 " +
+            "<OPTION VALUE='50'>50 " +
+            "<OPTION VALUE='1000'>All " +
+            "</select>";
+        }
+        if (results == 50){
+            rangeDropList = "<select name='results' " +
+            "onchange='this.form.submit();'> " +
+            "<OPTION VALUE='10'>10 " +
+            "<OPTION VALUE='25'>25 " +
+            "<OPTION VALUE='50' selected>50 " +
+            "<OPTION VALUE='1000'>All " +
+            "</select>";
+        }
+        if (results == 1000){
+            rangeDropList = "<select name='results' " +
+            "onchange='this.form.submit();'> " +
+            "<OPTION VALUE='10'>10 " +
+            "<OPTION VALUE='25'>25 " +
+            "<OPTION VALUE='50'>50 " +
+            "<OPTION VALUE='1000' selected>All " +
+            "</select>";
+        }
+	    return rangeDropList;
+	}
+
+
+    //----------------------------------
+    // boolean tests for anchor display
+    //----------------------------------
+
+	public boolean getShowStartLink() {
+        if (startIndex == 0) {return false;}
+	    return true;
+	}
+	public boolean getShowPreviousLink() {
+        if (startIndex == 0) {return false;}
+	    return true;
+	}
+	public boolean getShowNextLink() {
+        if (startIndex + results >= resultsTotal) {return false;}
+	    return true;
+	}
+	public boolean getShowLastLink() {
+        if (startIndex + results >= resultsTotal) {return false;}
+	    return true;
+	}
+
+}
