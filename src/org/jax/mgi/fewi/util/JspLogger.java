6e9b9c2cb2c8af9a3ff10dd74ddd406f5e1a61e1
Peter Frost <Pete.Frost@jax.org>
2011-11-04 16:28:07 +0000

    Initial version

---
+++ src/org/jax/mgi/fewi/util/JspLogger.java
@@ -0,0 +1,22 @@
+package org.jax.mgi.fewi.util;
+
+import java.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+* wrapper - provides methods to log flow of JSP pages, which don't log
+* normally, even in scriptlet
+*/
+public class JspLogger
+{
+    // logger for the class
+    private static Logger logger = LoggerFactory.getLogger(JspLogger.class);
+
+    public void log (String logString) {
+		logger.debug(logString);
+    }
+
+} // end of class
+
