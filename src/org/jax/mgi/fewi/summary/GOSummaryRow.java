c7b1379ce33953995f6b4e102482fcbf0a909128
Jonathan Beal <Jon.Beal@jax.org>
2012-05-18 19:13:15 +0000

    updated for new IDLinker usage

--- src/org/jax/mgi/fewi/summary/GOSummaryRow.java
+++ src/org/jax/mgi/fewi/summary/GOSummaryRow.java
@@ -1,13 +1,15 @@
 package org.jax.mgi.fewi.summary;
 
-import java.util.*;
+import java.util.List;
 
 import mgi.frontend.datamodel.Annotation;
 import mgi.frontend.datamodel.AnnotationInferredFromID;
 import mgi.frontend.datamodel.Marker;
 import mgi.frontend.datamodel.Reference;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.jax.mgi.fewi.config.ContextLoader;
+import org.springframework.stereotype.Component;
 import org.jax.mgi.fewi.util.IDLinker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +18,7 @@ import org.slf4j.LoggerFactory;
 /**
  * wrapper around an Annotation;  represents on row in summary
  */
+@Component
 public class GOSummaryRow {
 
 	//-------------------
@@ -24,23 +27,23 @@ public class GOSummaryRow {
 
     private Logger logger = LoggerFactory.getLogger(GOSummaryRow.class);
 
-	// config values
-    String fewiUrl = ContextLoader.getConfigBean().getProperty("FEWI_URL");
-    String wiUrl   = ContextLoader.getConfigBean().getProperty("WI_URL");
-
-	// encapsulated row objects
+	// encapsulated row object
 	private Annotation annot;
+	
 	private Marker marker;
-
-	// object used to generate the AccID anchors
+	
 	private IDLinker  linker = ContextLoader.getIDLinker();
+
+	// config values
+    String wiUrl = ContextLoader.getConfigBean().getProperty("WI_URL");
+    String fewiUrl = ContextLoader.getConfigBean().getProperty("FEWI_URL");
 
 
 	//-------------
 	// constructors
 	//-------------
 
-	// hide the default constructor - we NEED a both an annotation and marker
+	// hide the default constructor - we NEED a foo to wrap
     private GOSummaryRow () {}
 
     public GOSummaryRow (Annotation annot, Marker marker) {
@@ -54,73 +57,62 @@ public class GOSummaryRow {
     // public instance methods;  JSON serializer will call all public methods
     //------------------------------------------------------------------------
 
-    // CATAGORY
     public String getCategory() {
     	return annot.getDagName();
     }
-
-    // CLASSIFICATION
     public String getTerm() {
-
-    	String termText = "<a href='"+ wiUrl +"searches/GO.cgi?id=" + annot.getTermID()
-    	    + "'> " + annot.getTerm() + "</a>";
-
+    	String termText = "<a href='"+ wiUrl +"searches/GO.cgi?id=" + annot.getTermID() + "'> " + annot.getTerm() + "</a>";
     	if (annot.getQualifier() != null && annot.getQualifier().equals("NOT")) {
     		return "<b>NOT</b> " + termText;
     	}
         return termText;
     }
-
-    // EVIDENCE
     public String getEvidence() {
     	return annot.getEvidenceCode();
     }
-
-    // INFERRED FROM
     public String getInferred() {
-
         List<AnnotationInferredFromID> inferred = annot.getInferredFromList();
-
         if (inferred.size() >= 1)
         {
             String inferredString = "";
             Boolean first = Boolean.TRUE;
+            
+	    if (linker == null) {
+		linker = ContextLoader.getIDLinker();
+	    }
 
             for (AnnotationInferredFromID aifi: inferred) {
                 if (first) {
+			first = Boolean.FALSE;
                 	inferredString = linker.getLink(aifi.getLogicalDB(), aifi.getAccID());
-                	first = Boolean.FALSE;
                 }
                 else {
                 	inferredString = inferredString + " | " + linker.getLink(aifi.getLogicalDB(), aifi.getAccID());
                 }
             }
-
+            
             return inferredString;
         }
 
         return "";
     }
-
-    // REFERENCE
     public String getReferences() {
-
         List<Reference> references = annot.getReferences();
+        
         if (references == null || references.size() == 0) {
             return "";
         }
-
-        //deferred code to check the number of references.
+        
         String refString = "";
         Boolean first = Boolean.TRUE;
         for (Reference ref: references) {
             if (first) {
-                refString = "<a href='" + fewiUrl + "reference/" + ref.getJnumID()
+		refString = "<a href='" + fewiUrl + "reference/" + ref.getJnumID() 
                             + "'>" + ref.getJnumID() + "</a>";
                 first = Boolean.FALSE;
             }
             else {
-                refString = refString + ", <a href='" + fewiUrl + "reference/" + ref.getJnumID()
+		refString = refString + ", <a href='" + fewiUrl + "reference/" + ref.getJnumID() 
                             + "'>" + ref.getJnumID() + "</a>";
             }
         }
