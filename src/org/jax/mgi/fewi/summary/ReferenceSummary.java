127ed3e98372c239c267408b4494c478ac9d33ca
Kevin Stone <Kevin.Stone@jax.org>
2012-07-27 16:11:09 +0000

    manually merged. CVS is dumb

--- src/org/jax/mgi/fewi/summary/ReferenceSummary.java
+++ src/org/jax/mgi/fewi/summary/ReferenceSummary.java
@@ -28,8 +28,8 @@ public class ReferenceSummary {
 
 	public String getScore() {
 		return score;
-	}	
-	
+	}
+
 	public String getVol(){
 		StringBuffer sb = new StringBuffer();
 		if (this.reference.getVol() != null && !"".equals(this.reference.getVol())){
@@ -42,27 +42,27 @@ public class ReferenceSummary {
 		}
 		if (this.reference.getPages() != null && !"".equals(this.reference.getPages())){
 			sb.append(this.reference.getPages());
-		}		
+		}
 		return sb.toString();
 	}
 
-	public String getId() {		
+	public String getId() {
 		StringBuffer sb = new StringBuffer();
-		
+
 		String pId = reference.getPubMedID();
 		if(pId != null && !"".equals(pId)){
 			String p = pmUrl.replace("@@@@", pId);
-			sb.append(String.format("<a href=\"%s\" target=\"_blank\" class=\"extUrl\">%s</a><br/>", 
-					p, pId));			
+			sb.append(String.format("<a href=\"%s\" target=\"_blank\" class=\"extUrl\">%s</a><br/>",
+					p, pId));
 		}
 		sb.append(reference.getJnumID());
 		if (this.reference.getFullTextLink() != null) {
-			sb.append(String.format("<br/><a href=\"%s\" target=\"_blank\" class=\"extUrl\">Full Text</a>", 
+			sb.append(String.format("<br/><a href=\"%s\" target=\"_blank\" class=\"extUrl\">Full Text</a>",
 					this.reference.getFullTextLink()));
 		}
 		if(!"".equals(this.reference.getDoiId())){
 			String du = doiUrl.replace("@@@@", this.reference.getDoiId());
-			sb.append(String.format("<br/><a href=\"%s\" target=\"_blank\" class=\"extUrl\">Journal Link</a>", 
+			sb.append(String.format("<br/><a href=\"%s\" target=\"_blank\" class=\"extUrl\">Journal Link</a>",
 					du));
 		}
 		return sb.toString();
@@ -70,7 +70,7 @@ public class ReferenceSummary {
 
 	public String getAuthors() {
 		StringBuffer sb = new StringBuffer();
-		if ("BOOK".equalsIgnoreCase(this.reference.getReferenceType()) && 
+		if ("BOOK".equalsIgnoreCase(this.reference.getReferenceType()) &&
 				this.reference.getBookEditor() != null){
 			sb.append(String.format("<br/><br/><span class=\"ital\">Editors</span>: %s", this.reference.getBookEditor()));
 		}
@@ -93,7 +93,7 @@ public class ReferenceSummary {
 	}
 
 	public String getJournal() {
-		if("BOOK".equalsIgnoreCase(this.reference.getReferenceType()) && 
+		if("BOOK".equalsIgnoreCase(this.reference.getReferenceType()) &&
 				this.reference.getBookPublisher() != null){
 			StringBuffer sb = new StringBuffer();
 			sb.append("<span class=\"ital\">Publisher</span>: " + this.reference.getBookPublisher());
@@ -108,23 +108,17 @@ public class ReferenceSummary {
 	public String getYear() {
 		return reference.getYear();
 	}
-	
+
 	public String getCuratedData(){
 		StringBuffer sb = new StringBuffer();
-		
+
 		String fewiUrl = ContextLoader.getConfigBean().getProperty("FEWI_URL");
 		String wiUrl = ContextLoader.getConfigBean().getProperty("WI_URL");
-		
+
         sb.append("<ul class=\"curatedData\">");
-        
-        int expTotal = reference.getCountOfGXDAssays() + reference.getCountOfGXDResults();
-        if (expTotal > 0){
-        	sb.append(String.format("<li>Expression assays: <a href=\"%ssearches/expression_report.cgi?_Refs_key=%s&returnType=assays&sort=Gene symbol\">%,d</a>,", wiUrl, this.reference.getReferenceKey(), this.reference.getCountOfGXDAssays()));
-        	if (this.reference.getCountOfGXDResults().intValue() > 1000){       	
-        		sb.append(String.format(" results: %,d</li>", this.reference.getCountOfGXDResults()));
-        	} else {
-        		sb.append(String.format(" results: <a href=\"%ssearches/expression_report.cgi?_Refs_key=%s&returnType=assay results&sort=Gene symbol\">%,d</a></li>", wiUrl, this.reference.getReferenceKey(), this.reference.getCountOfGXDResults()));
-        	}
+
+        if (reference.getCountOfGXDResults() > 0){
+        	sb.append(String.format("<li>Expression results: <a href=\"%sgxd/reference/%s\">%,d</a></li>", fewiUrl, this.reference.getJnumID(), this.reference.getCountOfGXDResults()));
         }
         if (reference.getCountOfGXDIndex() > 0){
         	sb.append(String.format("<li>Expression literature records: <a href=\"%sgxdlit/reference/%s\">%,d</a></li>", fewiUrl, this.reference.getJnumID(), this.reference.getCountOfGXDIndex()));
@@ -142,16 +136,16 @@ public class ReferenceSummary {
         	sb.append(String.format("<li>Molecular probes and clones: <a href=\"%ssearches/probe_report.cgi?_Refs_key=%s\">%,d</a></li>", wiUrl, this.reference.getReferenceKey(), this.reference.getCountOfProbes()));
         }
         if(reference.getCountOfSequenceResults() >0){
-        	sb.append(String.format("<li>Sequences: <a href=\"%ssequence/reference/%s\">%,d</a></li>", fewiUrl, this.reference.getJnumID(), this.reference.getCountOfSequenceResults()));  
+        	sb.append(String.format("<li>Sequences: <a href=\"%ssequence/reference/%s\">%,d</a></li>", fewiUrl, this.reference.getJnumID(), this.reference.getCountOfSequenceResults()));
         }
-        sb.append("</ul>"); 		
-		
+        sb.append("</ul>");
+
 		return sb.toString();
 	}
-	
+
 	public String getAbstract(){
 		String abs = "";
-		if (this.reference.getAbstract() != null 
+		if (this.reference.getAbstract() != null
 				&& !"".equals(this.reference.getAbstract())){
 			abs = abstractHL.highLight(this.reference.getAbstract());
 		} else {
@@ -183,11 +177,11 @@ public class ReferenceSummary {
 	public void setTitleHL(Highlighter highlighter) {
 		this.titleHL = highlighter;
 	}
-	
+
 	public void setAbstractHL(Highlighter highlighter) {
 		this.abstractHL = highlighter;
 	}
-	
+
 	public void setAuthorHL(Highlighter highlighter) {
 		this.authorHL = highlighter;
 	}
