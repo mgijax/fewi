0135fe5e600e15ff051164023434c50f8731eeee
Jeffrey Campbell <Jeff.Campbell@jax.org>
2013-11-05 18:13:56 +0000

    new test HDPGridDiseaseColor.html

---
+++ test/diseasePortal/HDPGridDiseaseColorTest.java
@@ -0,0 +1,19 @@
+package diseasePortal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mgi.frontend.datamodel.Marker;
+
+import org.jax.mgi.fewi.controller.DiseasePortalController;
+import org.jax.mgi.fewi.searchUtil.SearchResults;
+import org.jax.mgi.fewi.searchUtil.entities.SolrVocTerm;
+import org.jax.mgi.fewi.test.base.HDPBaseConcordionTest;
+import org.jax.mgi.fewi.test.mock.MockHdpControllerQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class HDPGridDiseaseColorTest extends HDPBaseConcordionTest 
+{
+}
+	
