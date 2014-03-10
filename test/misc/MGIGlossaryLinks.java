7b3bf4a6224592635d8b7089a82f4e5ce03bb762
Kim Forthofer <Kim.Forthofer@jax.org>
2014-03-10 18:19:22 +0000

    renamed a package

--- test/misc/MGIGlossaryLinks.java
+++
@@ -1,9 +0,0 @@
-package misc;
-
-import org.jax.mgi.fewi.test.base.BaseConcordionTest;
-
-
-public class MGIGlossaryLinks extends BaseConcordionTest {
-
-}
-	
--- test/misc/QuickSearch.java
+++
@@ -1,9 +0,0 @@
-package misc;
-
-import org.jax.mgi.fewi.test.base.BaseConcordionTest;
-
-
-public class QuickSearch extends BaseConcordionTest {
-
-}
-	
