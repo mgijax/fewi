032a41e6dfe4fef4f9364ce0812b928da9bf14e1
Kim Forthofer <Kim.Forthofer@jax.org>
2014-03-10 18:18:22 +0000

    renamed package from misc to commeonElements, added index page

---
+++ test/commonElements/MouseMineLinks.java
@@ -0,0 +1,9 @@
+package commonElements;
+
+import org.jax.mgi.fewi.test.base.BaseConcordionTest;
+
+
+public class MouseMineLinks extends BaseConcordionTest {
+
+}
+	
---
+++ test/commonElements/QuickSearch.java
@@ -0,0 +1,9 @@
+package commonElements;
+
+import org.jax.mgi.fewi.test.base.BaseConcordionTest;
+
+
+public class QuickSearch extends BaseConcordionTest {
+
+}
+	
