b069b2455ea1adc03e1b5b73a963e917bd164db5
Kevin Stone <Kevin.Stone@jax.org>
2014-03-24 16:00:13 +0000

    adding multiple mcv keys

--- test/marker/MarkerSummaryFromQueryTest.java
+++ test/marker/MarkerSummaryFromQueryTest.java
@@ -10,6 +10,63 @@ import org.jax.mgi.fewi.test.mock.MockMarkerControllerQuery;
 import org.jax.mgi.fewi.util.QueryParser;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
+/*
+ * TIP: 
+ * Feature types must be mapped to term keys
+ * 
+ *              term                 | term_key 
+--------------------------------------+----------
+ all feature types                    |  6238159
+ BAC end                              |  6442608
+ BAC/YAC end                          |  6238177
+ chromosomal deletion                 |  7196768
+ chromosomal duplication              |  7196774
+ chromosomal inversion                |  7196770
+ chromosomal translocation            |  7196773
+ chromosomal transposition            |  7196775
+ complex/cluster/region               |  6238175
+ cytogenetic marker                   |  6238176
+ DNA segment                          |  6238179
+ endogenous retroviral region         |  9272146
+ gene                                 |  6238160
+ gene segment                         |  6238171
+ heritable phenotypic marker          |  6238170
+ insertion                            |  7196769
+ lincRNA gene                         |  6238169
+ minisatellite                        |  7648968
+ miRNA gene                           |  6238167
+ non-coding RNA gene                  |  6238162
+ other feature type                   |  6238185
+ other genome feature                 |  6238178
+ PAC end                              |  6442610
+ polymorphic pseudogene               |  7288449
+ protein coding gene                  |  6238161
+ pseudogene                           |  7313348
+ pseudogenic gene segment             |  6967235
+ pseudogenic region                   |  7288448
+ QTL                                  |  6238173
+ reciprocal chromosomal translocation |  7196772
+ retrotransposon                      |  7648966
+ RNase MRP RNA gene                   |  6238182
+ RNase P RNA gene                     |  6238181
+ Robertsonian fusion                  |  7196771
+ rRNA gene                            |  6238163
+ scRNA gene                           |  6238168
+ snoRNA gene                          |  6238166
+ snRNA gene                           |  6238165
+ SRP RNA gene                         |  6238180
+ telomerase RNA gene                  |  6238183
+ telomere                             |  7648967
+ transgene                            |  6238174
+ tRNA gene                            |  6238164
+ unclassified cytogenetic marker      |  7222413
+ unclassified gene                    |  6238184
+ unclassified non-coding RNA gene     |  6238186
+ unclassified other genome feature    |  7648969
+ YAC end                              |  6442609
+ */
+
 public class MarkerSummaryFromQueryTest extends BaseConcordionTest 
 {
 	@Autowired
@@ -31,16 +88,16 @@ public class MarkerSummaryFromQueryTest extends BaseConcordionTest
 	}
 	
 	// Feature type query
-	public int getMarkerCountByMcv(String mcvKey) throws Exception
+	public int getMarkerCountByMcv(String mcvKeys) throws Exception
 	{
 		MockMarkerControllerQuery mq = this.getMockQuery().markerController(markerController);
-		mq.setMcvKey(mcvKey);
+		mq.setMcvKey(decodeCommaValues(mcvKeys));
 		return mq.getMarkers().getTotalCount();
 	}
-	public List<String> getSymbolsByMcv(String mcvKey) throws Exception
+	public List<String> getSymbolsByMcv(String mcvKeys) throws Exception
 	{
 		MockMarkerControllerQuery mq = this.getMockQuery().markerController(markerController);
-		mq.setMcvKey(mcvKey);
+		mq.setMcvKey(decodeCommaValues(mcvKeys));
 		return getSymbolsByMQ(mq);
 	}
 	
@@ -48,13 +105,13 @@ public class MarkerSummaryFromQueryTest extends BaseConcordionTest
 	public int getMarkerCountByChromosome(String chrString) throws Exception
 	{
 		MockMarkerControllerQuery mq = this.getMockQuery().markerController(markerController);
-		mq.setChromosome(decodeChrs(chrString));
+		mq.setChromosome(decodeCommaValues(chrString));
 		return mq.getMarkers().getTotalCount();
 	}
 	public List<String> getSymbolsByChromosome(String chrString) throws Exception
 	{
 		MockMarkerControllerQuery mq = this.getMockQuery().markerController(markerController);
-		mq.setChromosome(decodeChrs(chrString));
+		mq.setChromosome(decodeCommaValues(chrString));
 		return getSymbolsByMQ(mq);
 	}
 	
@@ -154,9 +211,9 @@ public class MarkerSummaryFromQueryTest extends BaseConcordionTest
 		return symbols;
 	}
 	
-	private List<String> decodeChrs(String chrString)
+	private List<String> decodeCommaValues(String input)
 	{
-		return QueryParser.tokeniseOnWhitespaceAndComma(chrString);
+		return QueryParser.tokeniseOnWhitespaceAndComma(input);
 	}
 }
 	
