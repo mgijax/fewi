0c7b7ef0854bbeafaae335bf4e88e7895de2b702
Kevin Stone <Kevin.Stone@jax.org>
2014-04-17 16:30:15 +0000

    adding vcf file processor unit tests

--- src/unittest/fewi/util/file/FileProcessorTest.java
+++ src/unittest/fewi/util/file/FileProcessorTest.java
@@ -10,6 +10,7 @@ import junit.framework.Assert;
 import org.apache.commons.lang.StringUtils;
 import org.jax.mgi.fewi.util.file.FileProcessor;
 import org.jax.mgi.fewi.util.file.FileProcessorOutput;
+import org.jax.mgi.fewi.util.file.VcfProcessorOutput;
 import org.junit.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,7 +21,112 @@ import org.springframework.web.multipart.MultipartFile;
  */
 public class FileProcessorTest 
 {
+	/*
+	 * Tests for processVCFCoordinates()
+	 */
+	@Test
+	public void testVCFEmptyFile() throws IOException
+	{
+		VcfProcessorOutput po = FileProcessor.processVCFCoordinates(mockFile("test",""));
 
+		Assert.assertEquals(0,po.getRowsProcessed());
+		Assert.assertEquals(0,po.getRowsWithCoordinates());
+		Assert.assertEquals(0,po.getRowsKicked());
+		Assert.assertEquals(0,po.getRowsKickedWithNoData());
+		Assert.assertEquals(0,po.getRowsKickedWithNotPass());
+		Assert.assertEquals(0,po.getRowsKickedWithId());
+	}
+	
+	@Test
+	public void testVCFBlankRow() throws IOException
+	{
+		VcfProcessorOutput po = FileProcessor.processVCFCoordinates(mockFile("test","  "));
+
+		Assert.assertEquals(1,po.getRowsProcessed());
+		Assert.assertEquals(0,po.getRowsWithCoordinates());
+		Assert.assertEquals(1,po.getRowsKicked());
+		Assert.assertEquals(1,po.getRowsKickedWithNoData());
+		Assert.assertEquals(0,po.getRowsKickedWithNotPass());
+		Assert.assertEquals(0,po.getRowsKickedWithId());
+	}
+	
+	@Test
+	public void testVCFManyRows() throws IOException
+	{
+		VcfProcessorOutput po = FileProcessor.processVCFCoordinates(mockFile("test",
+				Arrays.asList("chr2\t3456\t.\tC\tT\t28601.2\tPASS",
+						"chr1\t2345\t.\tC\tT\t28601.2\tPASS",
+						"chr3\t4567\t.\tC\tT\t28601.2\tPASS",
+						"chr3\t4567",
+						"\t",
+						"chr1\t2345\t.\tT\t232323")
+						));
+
+		Assert.assertEquals(6,po.getRowsProcessed());
+		Assert.assertEquals(4,po.getRowsWithCoordinates());
+		Assert.assertEquals(2,po.getRowsKicked());
+		Assert.assertEquals(2,po.getRowsKickedWithNoData());
+		Assert.assertEquals(0,po.getRowsKickedWithNotPass());
+		Assert.assertEquals(0,po.getRowsKickedWithId());
+	}
+	
+	@Test
+	public void testVCFCommentRow() throws IOException
+	{
+		VcfProcessorOutput po = FileProcessor.processVCFCoordinates(mockFile("test",
+				Arrays.asList("chr2\t3456\t.\tC\tT\t28601.2\tPASS",
+						"#chr2\t3456\t.\tC\tT\t28601.2\tPASS")
+						));
+
+		Assert.assertEquals(2,po.getRowsProcessed());
+		Assert.assertEquals(1,po.getRowsWithCoordinates());
+		Assert.assertEquals(1,po.getRowsKicked());
+		Assert.assertEquals(1,po.getRowsKickedWithNoData());
+		Assert.assertEquals(0,po.getRowsKickedWithNotPass());
+		Assert.assertEquals(0,po.getRowsKickedWithId());
+	}
+	
+	@Test
+	public void testVCFNoPassRow() throws IOException
+	{
+		VcfProcessorOutput po = FileProcessor.processVCFCoordinates(mockFile("test",
+				Arrays.asList("chr2\t3456\t.\tC\tT\t28601.2\tPASS\t",
+						"chr2\t3456\t.\tC\tT\t28601.2\tfalse\t",
+						"chr2\t3456\t.\tC\tT\t28601.2\tfalse\t",
+						"chr2\t3456\t.\tC\tT\t28601.2\t\t",
+						"chr2\t3456\t.\tC\tT\t28601.2\t.\t",
+						"chr2\t3456\t.\tC\tT\t28601.2\tpass\t")
+						));
+
+		Assert.assertEquals(6,po.getRowsProcessed());
+		Assert.assertEquals(4,po.getRowsWithCoordinates());
+		Assert.assertEquals(2,po.getRowsKicked());
+		Assert.assertEquals(0,po.getRowsKickedWithNoData());
+		Assert.assertEquals(2,po.getRowsKickedWithNotPass());
+		Assert.assertEquals(0,po.getRowsKickedWithId());
+	}
+	
+	@Test
+	public void testVCFRowWithId() throws IOException
+	{
+		VcfProcessorOutput po = FileProcessor.processVCFCoordinates(mockFile("test",
+				Arrays.asList("chr2\t3456\t.\tC\tT\t28601.2\tPASS\t",
+						"chr2\t3456\t\tC\tT\t28601.2\tPASS\t",
+						"chr2\t3456\tID:12345\tC\tT\t28601.2\tfalse\t",
+						"chr2\t3456\t1345\tC\tT\t28601.2\t\t")
+						));
+
+		Assert.assertEquals(4,po.getRowsProcessed());
+		Assert.assertEquals(2,po.getRowsWithCoordinates());
+		Assert.assertEquals(2,po.getRowsKicked());
+		Assert.assertEquals(0,po.getRowsKickedWithNoData());
+		Assert.assertEquals(0,po.getRowsKickedWithNotPass());
+		Assert.assertEquals(2,po.getRowsKickedWithId());
+	}
+	
+	/*
+	 * Tests for processSingleCol()
+	 */
 	@Test
 	public void testSingleColEmptyFile() throws IOException
 	{
