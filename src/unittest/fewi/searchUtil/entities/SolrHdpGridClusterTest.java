ee829976f1cb0216e431a8ddb509fb283a8b8fc9
Kevin Stone <Kevin.Stone@jax.org>
2014-04-22 15:50:38 +0000

    changed test to make sure human marker can be missing feature type and still work

--- src/unittest/fewi/searchUtil/entities/SolrHdpGridClusterTest.java
+++ src/unittest/fewi/searchUtil/entities/SolrHdpGridClusterTest.java
@@ -48,18 +48,18 @@ public class SolrHdpGridClusterTest {
 	@Test
 	public void testParseHumanMarkers() {
 		SolrHdpGridCluster cluster = new SolrHdpGridCluster();
-		cluster.setHumanSymbols(Arrays.asList("Pax6||1234||Paired box 6||protein coding gene",
-				"Pax5||2345||Paired box 5||QTL"));
+		cluster.setHumanSymbols(Arrays.asList("Pax6||1234||Paired box 6||gene",
+				"Pax5||2345||Paired box 5"));
 		List<SolrDpGridClusterMarker> markers = cluster.getHumanMarkers();
 		Assert.assertEquals(2,markers.size());
 		Assert.assertEquals("Pax6",markers.get(0).getSymbol());
 		Assert.assertEquals("1234",markers.get(0).getMarkerKey());
 		Assert.assertEquals("Paired box 6",markers.get(0).getName());
-		Assert.assertEquals("protein coding gene",markers.get(0).getFeatureType());
+		Assert.assertEquals("gene",markers.get(0).getFeatureType());
 		Assert.assertEquals("Pax5",markers.get(1).getSymbol());
 		Assert.assertEquals("2345",markers.get(1).getMarkerKey());
 		Assert.assertEquals("Paired box 5",markers.get(1).getName());
-		Assert.assertEquals("QTL",markers.get(1).getFeatureType());
+		Assert.assertEquals("",markers.get(1).getFeatureType());
 	}
 
 }
