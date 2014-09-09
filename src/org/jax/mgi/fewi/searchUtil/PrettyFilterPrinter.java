204946a96c364b9a56d79ab840817291231850be
Olin Blodgett <Olin.Blodgett@jax.org>
2014-09-09 21:01:23 +0000

    Added files for pretty filter printing

---
+++ src/org/jax/mgi/fewi/searchUtil/PrettyFilterPrinter.java
@@ -0,0 +1,36 @@
+package org.jax.mgi.fewi.searchUtil;
+
+
+public class PrettyFilterPrinter extends PrinterUtil implements VisitorInterface {
+
+
+	public void Visit(Filter filter) {
+		
+		if(filter.getNestedFilters().size() > 0) {
+			if(filter.getFilterJoinClause() == Filter.JoinClause.FC_AND) printni("(");
+			int c = 0;
+			for(Filter f: filter.getNestedFilters()) {
+				if(filter.getFilterJoinClause() == Filter.JoinClause.FC_AND) {
+					printni("("); f.Accept(this); printni(")");
+				} else {
+					f.Accept(this);
+				}
+				c++;
+				if(c < filter.getNestedFilters().size()) {
+					printni(" " + filter.getFilterJoinClause().getName() + " ");
+				}
+			}
+			if(filter.getFilterJoinClause() == Filter.JoinClause.FC_AND) printni(")");
+		} else {
+			if(filter.doNegation()) {
+				printni("NOT ");
+			}
+			if(filter.getValue().split(" ").length > 1) { 
+				printni("\"" + filter.getValue() + "\"");
+			} else {
+				printni(filter.getValue());
+			}
+		}
+	}
+
+}
