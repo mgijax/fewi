58252de10d38e91042c67dddb40b227dc1f092d1
Olin Blodgett <nilo@nilosplace.net>
2016-04-14 15:09:42 -0400

    Added natural sorting to the smart table

--- www/hmdc/search/index.js
+++ www/hmdc/search/index.js
@@ -8,12 +8,13 @@
     'civic.services',
     'civic.search',
     'civic.style',
+    'naturalSort',
   ]);
 
 // define root modules & dependencies
   angular.module('civic.config', ['formly', 'formlyBootstrap']);
   angular.module('civic.services', ['ngResource']);
-  angular.module('civic.search', ['ngRoute', 'formly', 'formlyBootstrap', 'ui.bootstrap', 'angular-table', 'ngDialog', 'smart-table']);
+  angular.module('civic.search', ['ngRoute', 'formly', 'formlyBootstrap', 'ui.bootstrap', 'angular-table', 'ngDialog', 'smart-table', 'naturalSort']);
   angular.module('civic.style', ['ngRoute', 'formly', 'formlyBootstrap', 'ui.bootstrap', 'angular-table', 'ngDialog', 'smart-table']);
 
 
