990a9f8e8109fb2103e19df794f07805dccec1f0
Olin Blodgett <nilo@nilosplace.net>
2016-07-12 13:18:48 -0400

    Added mac slash r's to the line endings

--- www/hmdc/app/components/search/SearchController.js
+++ www/hmdc/app/components/search/SearchController.js
@@ -152,7 +152,7 @@
 
 			var geneList = [];
 			if(scope.model.fileData && scope.model.fileData.length > 0) {
-				var lines = scope.model.fileData.split(/\r\n|\n/);
+				var lines = scope.model.fileData.split(/\r\n|\r|\n/);
 				var i = 0;
 				if(scope.model.parameters[2]) {
 					i = 1;
