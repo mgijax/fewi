d3d7379ace464cffbc17f0865cfb6b75cae0cf4e
pf <peter.frost@jax.org>
2018-01-23 08:25:37 -0500

    Column header handling

--- www/js/src/gxd_pheno_matrix.js
+++ www/js/src/gxd_pheno_matrix.js
@@ -1,10 +1,11 @@
+// setup initial query string for requested marker
+var querystring = "markerMgiId=" + mrkID;
 
+// constants
 var LOADING_IMG_SRC = "/fewi/mgi/assets/images/loading.gif";
 var LOADING_IMG = "<img src=\""+LOADING_IMG_SRC+"\" height=\"24\" width=\"24\">";
 var SHOW_MATRIX_LEGENDS = false;
 
-// setup initial query string for requested marker
-var querystring = "markerMgiId=" + mrkID;
 
 // TODO - remove this if filters aren't needed;  refactor calls to this function 
 function getQueryStringWithFilters() {
@@ -20,6 +21,121 @@ function getQueryStringWithFilters() {
 	}
 	return querystringWithFilters;
 }
+
+
+
+
+
+
+function resolveGridColorClass(cell)
+{
+	var cc = "gold";
+	if(cell.detected>0)
+	{
+		// apply the detected bin
+		if(cell.detected < 5)
+		{
+			cc = "blue1";
+		}
+		else if(cell.detected < 51)
+		{
+			cc = "blue2";
+		}
+		else
+		{
+			cc = "blue3";
+		}
+	}
+	else if(cell.ambiguous>0) cc = "gray";
+	else if(cell.notDetected>0)
+	{
+		// apply the not detected bin
+		if(cell.notDetected < 5)
+		{
+			cc = "red1";
+		}
+		else if(cell.notDetected < 21)
+		{
+			cc = "red2";
+		}
+		else
+		{
+			cc = "red3";
+		}
+	}
+	return cc;
+}
+
+//rendering function for grid cells
+function StructurePhenoCellRenderer(d3Target,cellSize,cell){
+
+	var g = d3Target;
+	var fillClass = resolveGridColorClass(cell);
+
+	if ( fillClass == "gold" ) {
+		var points = [(cellSize - (cellSize/1.5))+","+0,
+	    	(cellSize)+","+0,
+	    	(cellSize)+","+(cellSize/1.5)];
+		g.append("polygon")
+			.attr("points",points.join(" "))
+			.attr("class",fillClass);
+		g.append("rect")
+			.attr("x",0)
+			.attr("y",0)
+			.attr("width",cellSize)
+			.attr("height",cellSize)
+			.style("fill","transparent");
+	}
+	else {
+		g.append("rect")
+			.attr("x",0)
+			.attr("y",0)
+			.attr("width",cellSize)
+			.attr("height",cellSize)
+			.style("stroke","#ccc")
+			.style("stroke-width","1px")
+			.attr("class",fillClass);
+
+		if(indicateNegativeMatrixResultsConflict(cell)){
+			addNegativeResultConflictIndicator(g,cellSize);
+		}
+	}
+
+	return g;
+};
+
+
+function StructurePhenoColumnHeaderRenderer(d3Target,cellSize)
+{
+	var labelPaddingLeft = 4;
+	var labelPaddingBottom = 4;
+    return  d3Target.append("text")
+    	.attr("x", 0)
+    	.attr("y",cellSize-labelPaddingBottom)
+    	.text(function(d){ return d.cid;})
+    	.style("font-size",_self.columnHeaderFontSize+"px")
+    	.style("font-weight","bold");
+}
+
+
+window.PhenoMatrixRender = new function()
+{
+	/*
+	 * Renderer for Theiler Stage columns
+	 */
+	this.StructurePhenoColumnHeaderRenderer = function(d3Target,cellSize,startX,startY)
+	{
+		var labelPaddingLeft = 4;
+		var labelPaddingBottom = 4;
+	    return  d3Target.append("text")
+	    	.attr("x", 0)
+	    	.attr("y",cellSize-labelPaddingBottom)
+	    	.text(function(d){ return d.colDisplay;})
+	    	.style("font-size","12px")
+	    	.style("font-weight","bold");
+	};
+}
+
 
 
 /**
@@ -52,8 +168,23 @@ var phenoSuperGrid = function()
 				MSG_EMPTY: 'No results found.'
 			},
 			cellSize: 24,
-			cellRenderer: GxdRender.StructureGeneCellRenderer,
-			columnSort: function(a,b){ return FewiUtil.SortSmartAlpha(a.cid,b.cid);},
+			columnRenderer: PhenoMatrixRender.StructurePhenoColumnHeaderRenderer,
+//			columnRenderer: function(d3Target,cellSize){ 
+//				var labelPaddingLeft = 4;
+//				var labelPaddingBottom = 4;
+//			    return  d3Target.append("text")
+//			    	.attr("x", 0)
+//			    	.attr("y",cellSize-labelPaddingBottom)
+//			    	.text(function(d){ return d.cid;})
+//			    	.style("font-size",_self.columnHeaderFontSize+"px")
+//			    	.style("font-weight","bold");
+//			},
+			cellRenderer: StructurePhenoCellRenderer,
+			columnSort: function(a,b){ 
+				if(a.cid>b.cid) return 1;
+				else if (a.cid<b.cid) return -1;
+				return 0;
+			},
 			verticalColumnLabels: true,
 	        openCloseStateKey: "gg_"+querystring,
 	        legendClickHandler: function(e){ geneMatrixLegendPopupPanel.show(); },
