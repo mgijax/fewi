c243e87e15802387b2eda7a097bdfa3489f5e10c
Jonathan Beal <jsb@jax.org>
2018-04-04 13:37:11 -0400

    wording tweaks per GXD

--- www/js/src/gxd_query.js
+++ www/js/src/gxd_query.js
@@ -268,6 +268,10 @@ var updateQuerySummary = function() {
 	// handle the differential stuff first
 	if(currentQF == 'differential') {
 		var el = new YAHOO.util.Element(document.createElement('span'));
+		// parse the structures input
+		var structure = YAHOO.util.Dom.get('difStructure3').value;
+		var notStructure = YAHOO.util.Dom.get('difStructure4').value;
+		
 		// parse the stages input
 		var selectedStages = parseStageOptions("difTheilerStage3","0");
 		var detectedStages = [];
@@ -283,10 +287,23 @@ var updateQuerySummary = function() {
 		}
 		var selectedDifStages = parseStageOptions("difTheilerStage4","-1");
 		var notDetectedStages = [];
-		var notDetectedStagesText = " any of the developmental stage(s):";
-		if(selectedDifStages=="Any") notDetectedStagesText = "<b>Any developmental stage not selected above</b>";
-		else
-		{
+		var notDetectedStagesText = "any of the developmental stage(s):";
+		if(selectedDifStages=="Any") {
+			// cases:
+			// 1. structure & notStructure, no selectedStages
+			// 2. structure & selectedStages & (structure == notStructure)
+			// 3. structure & selectedStages & notStructure
+			// 4. other
+			if ((structure != '') && (notStructure != '') && (selectedStages == 'Any')) {
+				notDetectedStagesText = "<b>Any developmental stage</b>";
+			} else if ((structure != '') && (selectedStages != 'Any') && (selectedStages != '') && (structure == notStructure)) {
+				notDetectedStagesText = "<b>Any developmental stage not selected above</b>";
+			} else if ((structure != '') && (selectedStages != 'Any') && (selectedStages != '') && (notStructure != '')) {
+				notDetectedStagesText = "<b>Any developmental stage</b>";
+			} else {
+				notDetectedStagesText = "<b>Any developmental stage not selected above</b>";
+			}
+		} else {
 			for(var i=0;i<selectedDifStages.length;i++)
 			{
 				notDetectedStages.push("<b>TS:"+selectedDifStages[i]+"</b>");
