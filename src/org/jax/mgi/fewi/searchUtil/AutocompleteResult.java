663daca2b0be95711924d888bb2b1162d86af82d
Jonathan Beal <jsb@jax.org>
2018-05-21 08:00:54 -0400

    fixed A/C bug on scrumdev by removing AutocompleteResultComparator and associated getter

--- src/org/jax/mgi/fewi/searchUtil/AutocompleteResult.java
+++ src/org/jax/mgi/fewi/searchUtil/AutocompleteResult.java
@@ -36,22 +36,4 @@ public class AutocompleteResult {
 	public void setValue(String value) {
 		this.value = value;
 	}
-	
-//	public AutocompleteResultComparator getComparator() {
-//		return new AutocompleteResultComparator();
-//	}
-
-/*	public class AutocompleteResultComparator implements Comparator<AutocompleteResult> {
-		@Override
-		public int compare(AutocompleteResult a, AutocompleteResult b) {
-			// assumes 'value' for 'a' and 'b' must be non-null
-			int i = smartAlphaComparator.compare(a.getValue(), b.getValue());
-			if (i == 0) {
-				// values match, so sort by labels
-				i = smartAlphaComparator.compare(a.getLabel(), b.getLabel());
-			}
-			return i;
-		}
-	}
-*/
 }
