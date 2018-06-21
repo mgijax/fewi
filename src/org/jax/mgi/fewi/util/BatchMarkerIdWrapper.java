926e1e9a98411ee3e780375b5900ccebb592ee68
Jonathan Beal <jsb@jax.org>
2018-06-21 15:41:10 -0400

    added protection against null pointer exceptions when marker's don't have strains

--- src/org/jax/mgi/fewi/util/BatchMarkerIdWrapper.java
+++ src/org/jax/mgi/fewi/util/BatchMarkerIdWrapper.java
@@ -71,10 +71,10 @@ public class BatchMarkerIdWrapper {
 	
 	// both markers and strain markers have associated strains
 	public String getStrain() {
-		if (isMarkerMatch()) {
+		if (isMarkerMatch() && (m.getStrain() != null)) {
 			// canonical markers always link to C57BL/6J
 			return strainLink.replace("(id)", "MGI:3028467").replaceAll("(name)", m.getStrain());
-		} else if (isStrainMarkerMatch()) {
+		} else if (isStrainMarkerMatch() && (sm.getStrainName() != null)) {
 			// strain markers link to their particular strain
 			return strainLink.replace("(id)", sm.getStrainID()).replaceAll("(name)", sm.getStrainName());
 		}
