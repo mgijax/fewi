11207b47c9497c2d89812657ad80857e2cd7cf23
Jonathan Beal <jsb@jax.org>
2018-07-03 16:21:32 -0400

    fixed MGP sequence links to also embed strain in URL

--- src/org/jax/mgi/fewi/util/link/ProviderLinker.java
+++ src/org/jax/mgi/fewi/util/link/ProviderLinker.java
@@ -30,7 +30,7 @@ public class ProviderLinker
 	private static String niaUrl = "http://lgsun.grc.nia.nih.gov/geneindex/mm9/bin/giU.cgi?genename=";
 	private static String refSeqUrl = "https://www.ncbi.nlm.nih.gov/entrez/viewer.cgi?val=";
 	private static String dotsUrl = "http://genomics.betacell.org/gbco/showSummary.do?questionFullName=TranscriptQuestions.TranscriptFromDtIds&myProp%28dtIdP%29=";
-	private static String mgpSeqUrl = "http://useast.ensembl.org/Mus_musculus_A_J/Gene/Summary?db=core;g=";
+	private static String mgpSeqUrl = "http://useast.ensembl.org/Mus_musculus_<strain>/Gene/Summary?db=core;g=<id>";
 
     /*-------------------------*/
     /* public instance methods */
@@ -95,8 +95,17 @@ public class ProviderLinker
 
 			links.append("<a href='" + refSeqUrl + seqID + "'>RefSeq</a>");
 		} else if (seqProvider.equals(DBConstants.PROVIDER_MGP)) {
+			
+			// need to pull the strain out of the ID, then insert it into the URL, and insert the ID into the URL
+			String[] pieces = seqID.split("_");
+			if (pieces.length == 3) {
+				String myUrl = mgpSeqUrl.replace("<strain>", tweakStrainValue(pieces[1])).replaceAll("<id>", seqID);
+				links.append("<a href='" + myUrl + "'>Mouse Genomes Project</a>");
+			} else {
+				// bad ID format, just show the provider
+				links.append("Mouse Genomes Project");
+			}
 
-			links.append("<a href='" + mgpSeqUrl + seqID + "'>Mouse Genomes Project</a>");
 		} else if (seqProvider.equals(DBConstants.PROVIDER_MGI_SGM)) {
 			
 			// no extra link for MGI C57BL/6J Strain Gene Model; just use the standard sequence detail link
@@ -105,6 +114,34 @@ public class ProviderLinker
         return links.toString();
     }
 
-
+    /* The strain we extract from the MGP ID is not always what we want to plug into the middle of mgpSeqUrl;
+     * sometimes we need to tweak it a bit.
+     */
+    public static String tweakStrainValue(String strain) {
+    	if (strain.equals("129S1SvImJ")) {			// 129S1/SvImJ
+    		return "129S1_SvImJ";
+    	} else if (strain.endsWith("EiJ")) {		// CAROLI/EiJ, CAST/EiJ, SPRET/EiJ, WSB/EiJ    
+    		return strain.replace("EiJ", "_EiJ");
+    	} else if (strain.equals("PWKPhJ")) {		// PWK/PhJ    
+    		return "PWK_PhJ";
+    	} else if (strain.equals("NODShiLtJ")) {	// NOD/ShiLtJ 
+    		return "NOD_ShiLtJ";
+    	} else if (strain.equals("NZOHlLtJ")) {		// NZO/HlLtJ  
+    		return "NZO_HlLtJ";
+    	} else if (strain.equals("C57BL6NJ")) {		// C57BL/6NJ  
+    		return "C57BL_6NJ";
+    	} else if (strain.equals("FVBNJ")) {		// FVB/NJ     
+    		return "FVB_NJ";
+    	} else if (strain.equals("DBA2J")) {		// DBA/2J     
+    		return "DBA_2J";
+    	} else if (strain.equals("C3HHeJ")) {		// C3H/HeJ
+    		return "C3H_HeJ";    
+    	} else if (strain.equals("BALBcJ")) {		// BALB/cJ    
+    		return "BALB_cJ";    
+    	} else if (strain.endsWith("J")) {			// A/J, AKR/J, CBA/J, LP/J       
+    		return strain.replace("J", "_J");
+    	}
+    	return strain;
+    }
 } // end of class ProviderLinker
 
