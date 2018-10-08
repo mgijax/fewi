d635317d588c009e9ba9491070ae3d939f610c7a
Jonathan Beal <jsb@jax.org>
2018-10-08 16:16:58 -0400

    adjusted sequence provider for MGP strain genes (to restore Ensembl links on sequence summary and detail pages)

--- src/org/jax/mgi/fewi/util/DBConstants.java
+++ src/org/jax/mgi/fewi/util/DBConstants.java
@@ -20,7 +20,7 @@ public class DBConstants
     public static final String PROVIDER_ENSEMBLPROTEIN    = "Ensembl Protein";
     public static final String PROVIDER_ENSEMBLTRANSCRIPT = "Ensembl Transcript";
     public static final String PROVIDER_UNIPROT	          = "Ensembl Transcript";
-    public static final String PROVIDER_MGP		          = "MGP Strain Gene Model";
+    public static final String PROVIDER_MGP		          = "Wellcome Sanger Institute's Mouse Genomes Project (MGP) Strain Gene Model";
     public static final String PROVIDER_MGI_SGM			  = "MGI C57BL/6J Strain Gene Model";
 }
 
