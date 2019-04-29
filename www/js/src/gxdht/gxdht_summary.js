8463f1bd49a4a7ae5dbf5e5a1c783ccc0b99f2e1
Jonathan Beal <jsb@jax.org>
2019-04-29 11:15:45 -0400

    fixed formatting of You Searched For to accommodate IDs with colons and underscores

--- www/js/src/gxdht/gxdht_summary.js
+++ www/js/src/gxdht/gxdht_summary.js
@@ -14,7 +14,7 @@ var instantiatedPaginator = false;
 var fewiUrl = null;
 
 var decode = function(s) {
-	return s.replace(/\+/g, ' ').replace(/[^A-Za-z0-9 ]/, ' ');
+	return decodeURIComponent(s).replace(/[^A-Za-z0-9:_ ]/, ' ');
 };
 
 // translate the float ages to have a prefixed 'E'
