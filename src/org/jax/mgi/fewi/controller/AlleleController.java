53cbc968f09ae9c171a7c97f0d69347d2d8cf061
Jonathan Beal <jsb@jax.org>
2019-05-02 16:00:27 -0400

    added user traffic monitoring to the /allele pages

--- src/org/jax/mgi/fewi/controller/AlleleController.java
+++ src/org/jax/mgi/fewi/controller/AlleleController.java
@@ -58,6 +58,7 @@ import org.jax.mgi.fewi.util.FilterUtil;
 import org.jax.mgi.fewi.util.FormatHelper;
 import org.jax.mgi.fewi.util.NotesTagConverter;
 import org.jax.mgi.fewi.util.QueryParser;
+import org.jax.mgi.fewi.util.UserMonitor;
 import org.jax.mgi.fewi.util.link.IDLinker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -131,6 +132,10 @@ public class AlleleController {
 
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView getQueryForm(HttpServletRequest request, HttpServletResponse response, @ModelAttribute AlleleQueryForm query) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
+
 		ModelAndView mav = new ModelAndView("allele_query");
 
 		initQFCache();
@@ -147,6 +152,10 @@ public class AlleleController {
 
 	@RequestMapping("/phenoPopup")
 	public ModelAndView phenoPopup (HttpServletRequest request) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
+
 		logger.debug("->systemPopup started");
 		String formName = request.getParameter("formName");
 
@@ -160,6 +169,10 @@ public class AlleleController {
 
 	@RequestMapping(value="/summary", method=RequestMethod.GET)
 	public ModelAndView alleleSummary(HttpServletRequest request, HttpServletResponse response, @ModelAttribute AlleleQueryForm query) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
+
 		ModelAndView mav = new ModelAndView("allele_summary");
 
 		// direct to marker summary if marker is in link
@@ -193,6 +206,10 @@ public class AlleleController {
 	//------------------------------------//
 	@RequestMapping(value="/reference/{refID}")
 	public ModelAndView alleleSummeryByRefId(HttpServletRequest request, @PathVariable("refID") String refID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
+
 		logger.debug("->alleleSummeryByRefId started");
 
 		// setup view object
@@ -237,6 +254,9 @@ public class AlleleController {
 
 	@RequestMapping(value="/{alleleID:.+}", method=RequestMethod.GET)
 	public ModelAndView alleleDetailByID(HttpServletRequest request, HttpServletResponse response, @PathVariable("alleleID") String alleleID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->alleleDetailByID started");
 
@@ -256,6 +276,9 @@ public class AlleleController {
 
 	@RequestMapping(value="/key/{dbKey:.+}", method=RequestMethod.GET)
 	public ModelAndView alleleDetailByKey(HttpServletRequest request, HttpServletResponse response, @PathVariable("dbKey") String dbKey) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->alleleDetailByKey started");
 
@@ -281,6 +304,9 @@ public class AlleleController {
 
 	@RequestMapping(value="/mutationInvolves/{alleleID:.+}", method=RequestMethod.GET)
 	public ModelAndView mutationInvolvesByID(HttpServletRequest request, HttpServletResponse response, @PathVariable("alleleID") String alleleID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->mutationInvolvesByID started");
 
@@ -446,6 +472,9 @@ public class AlleleController {
 	//--------------------------------//
 	@RequestMapping("/report*")
 	public ModelAndView alleleSummaryExport(HttpServletRequest request, @ModelAttribute AlleleQueryForm query, @ModelAttribute Paginator page) throws RecognitionException {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("generating report");
 
@@ -1368,6 +1397,9 @@ public class AlleleController {
 
 	@RequestMapping(value="/phenotable/{allID}")
 	public ModelAndView phenoTableByAllId(HttpServletRequest request,HttpServletResponse response, @PathVariable("allID") String allID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->phenoTableByAllId started");
 
@@ -1432,6 +1464,9 @@ public class AlleleController {
 	 */
 	@RequestMapping(value="/genoview/{genoID}")
 	public ModelAndView genoview(HttpServletRequest request, HttpServletResponse response, @PathVariable("genoID") String genoID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->genoview started");
 
@@ -1516,6 +1551,9 @@ public class AlleleController {
 
 	@RequestMapping(value="/allgenoviews/{alleleID}")
 	public ModelAndView allGenoviews(HttpServletRequest request, HttpServletResponse response, @PathVariable("alleleID") String alleleID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->all genoviews started");
 
@@ -1556,6 +1594,9 @@ public class AlleleController {
 
 	@RequestMapping(value="/alldiseasegenoviews/{alleleID}")
 	public ModelAndView allDiseaseGenoviews(HttpServletRequest request, HttpServletResponse response, @PathVariable("alleleID") String alleleID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->all disease genoviews started");
 
@@ -1599,6 +1640,9 @@ public class AlleleController {
 	//---------------------//
 	@RequestMapping(value="/diseasetable/{allID}")
 	public ModelAndView diseaseTableByAllId(HttpServletRequest request,HttpServletResponse response, @PathVariable("allID") String allID) {
+		if (!UserMonitor.getSharedInstance().isOkay(request.getRemoteAddr())) {
+			return UserMonitor.getSharedInstance().getLimitedMessage();
+		}
 
 		logger.debug("->diseaseTableByAllId started");
 
