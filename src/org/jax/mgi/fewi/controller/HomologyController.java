6764cf581ba7cba4e7f944094549d29755243bf3
jsb <jsb@jax.org>
2021-04-15 16:53:40 -0400

    shrunk flank down to 20% for mouse genes on homology page (in MGV link)

--- src/org/jax/mgi/fewi/controller/HomologyController.java
+++ src/org/jax/mgi/fewi/controller/HomologyController.java
@@ -223,8 +223,8 @@ public class HomologyController {
 			MarkerLocation mouseCoords = mouseMarker.getPreferredCoordinates();
 			if (mouseCoords != null) {
 				try {
-					// take half of the marker size and add to each end for flank (so roughly 50kb for a 100kb gene)
-					long flank = (new Double((mouseCoords.getEndCoordinate() - mouseCoords.getStartCoordinate()) / 2)).longValue();
+					// take 20% of the marker size and add to each end for flank (so roughly 50kb for a 100kb gene)
+					long flank = (new Double((mouseCoords.getEndCoordinate() - mouseCoords.getStartCoordinate()) / 5)).longValue();
 					
 					String chromosome = mouseCoords.getChromosome();
 					String startCoord = String.format("%.0f", mouseCoords.getStartCoordinate() - flank);
