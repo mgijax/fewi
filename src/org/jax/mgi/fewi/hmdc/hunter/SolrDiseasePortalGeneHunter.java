6485c3e78ad68c6fe4d71e873d949fa8bb7a8a51
jsb <jsb@jax.org>
2021-07-01 08:34:15 -0400

    updated to pull filterableFeatureTypes field from HMDC gene index and pass it along to the browser with each marker

--- src/org/jax/mgi/fewi/hmdc/hunter/SolrDiseasePortalGeneHunter.java
+++ src/org/jax/mgi/fewi/hmdc/hunter/SolrDiseasePortalGeneHunter.java
@@ -36,6 +36,7 @@ public class SolrDiseasePortalGeneHunter extends SolrHunter<SolrHdpEntityInterfa
 		returnedFields.add(DiseasePortalFields.MARKER_SYMBOL);
 		returnedFields.add(DiseasePortalFields.MARKER_MGI_ID);
 		returnedFields.add(DiseasePortalFields.MARKER_FEATURE_TYPE);
+		returnedFields.add(DiseasePortalFields.FILTERABLE_FEATURE_TYPES);
 		returnedFields.add(DiseasePortalFields.LOCATION_DISPLAY);
 		returnedFields.add(DiseasePortalFields.COORDINATE_DISPLAY);
 		returnedFields.add(DiseasePortalFields.BUILD_IDENTIFIER);
@@ -50,7 +51,6 @@ public class SolrDiseasePortalGeneHunter extends SolrHunter<SolrHdpEntityInterfa
 		
 	}
 
-	
 	
 	@Override
 	protected void packInformationByGroup(QueryResponse rsp, SearchResults<SolrHdpEntityInterface> sr, SearchParams sp) {
@@ -89,6 +89,9 @@ public class SolrDiseasePortalGeneHunter extends SolrHunter<SolrHdpEntityInterfa
 			marker.setSymbol((String)doc.getFieldValue(DiseasePortalFields.MARKER_SYMBOL));
 			marker.setMgiId((String)doc.getFieldValue(DiseasePortalFields.MARKER_MGI_ID));
 			marker.setType((String)doc.getFieldValue(DiseasePortalFields.MARKER_FEATURE_TYPE));
+			if (null != doc.getFieldValue(DiseasePortalFields.FILTERABLE_FEATURE_TYPES)) {
+				marker.setFilterableFeatureType((List<String>) doc.getFieldValue(DiseasePortalFields.FILTERABLE_FEATURE_TYPES));
+			}
 			marker.setLocation((String)doc.getFieldValue(DiseasePortalFields.LOCATION_DISPLAY));
 			marker.setCoordinate((String)doc.getFieldValue(DiseasePortalFields.COORDINATE_DISPLAY));
 			marker.setCoordinateBuild((String)doc.getFieldValue(DiseasePortalFields.BUILD_IDENTIFIER));
@@ -111,57 +114,8 @@ public class SolrDiseasePortalGeneHunter extends SolrHunter<SolrHdpEntityInterfa
 		}
 	}
 
-//	@Override
-//	protected void packInformation(QueryResponse rsp, SearchResults<SolrHdpEntityInterface> sr, SearchParams sp) {
-//
-//		SolrDocumentList sdl = rsp.getResults();
-//
-//		List<String> keys = new ArrayList<String>();
-//
-//		for (SolrDocument doc : sdl) {
-//
-//			Integer markerKey = (Integer)doc.getFieldValue(DiseasePortalFields.MARKER_KEY);
-//			// return just the marker key for now
-//			//sr.addResultObjects(markerKey);
-//			if(markerKey != null) keys.add(markerKey.toString());
-//			String markerKeyString = markerKey != null ? markerKey.toString() : "";
-//
-//			SolrHdpMarker marker = new SolrHdpMarker();
-//			marker.setMarkerKey(markerKey.toString());
-//			marker.setOrganism((String)doc.getFieldValue(DiseasePortalFields.ORGANISM));
-//			marker.setGridClusterKey((Integer)doc.getFieldValue(DiseasePortalFields.GRID_CLUSTER_KEY));
-//			marker.setHomologyClusterKey((Integer)doc.getFieldValue(DiseasePortalFields.HOMOLOGY_CLUSTER_KEY));
-//			marker.setHomologeneId((String)doc.getFieldValue(DiseasePortalFields.HOMOLOGENE_ID));
-//			marker.setHomologySource((String)doc.getFieldValue(DiseasePortalFields.HOMOLOGY_SOURCE));
-//			marker.setName((String)doc.getFieldValue(DiseasePortalFields.MARKER_NAME));
-//			marker.setSymbol((String)doc.getFieldValue(DiseasePortalFields.MARKER_SYMBOL));
-//			marker.setMgiId((String)doc.getFieldValue(DiseasePortalFields.MARKER_MGI_ID));
-//			marker.setType((String)doc.getFieldValue(DiseasePortalFields.MARKER_FEATURE_TYPE));
-//			marker.setLocation((String)doc.getFieldValue(DiseasePortalFields.LOCATION_DISPLAY));
-//			marker.setCoordinate((String)doc.getFieldValue(DiseasePortalFields.COORDINATE_DISPLAY));
-//			marker.setCoordinateBuild((String)doc.getFieldValue(DiseasePortalFields.BUILD_IDENTIFIER));
-//			marker.setDisease((List<String>)doc.getFieldValue(DiseasePortalFields.MARKER_DISEASE));
-//			marker.setMouseSystem((List<String>)doc.getFieldValue(DiseasePortalFields.MOUSE_MARKER_SYSTEM));
-//			marker.setHumanSystem((List<String>)doc.getFieldValue(DiseasePortalFields.HUMAN_MARKER_SYSTEM));
-//			marker.setAllRefCount((Integer)doc.getFieldValue(DiseasePortalFields.MARKER_ALL_REF_COUNT));
-//			marker.setDiseaseRefCount((Integer)doc.getFieldValue(DiseasePortalFields.MARKER_DISEASE_REF_COUNT));
-//			marker.setImsrCount((Integer)doc.getFieldValue(DiseasePortalFields.MARKER_IMSR_COUNT));
-//			keys.add(markerKeyString);
-//			sr.addResultObjects(marker);
-//		}
-//
-//		if (keys != null) {
-//			sr.setResultKeys(keys);
-//		}
-//	}
-
 	@Value("${solr.dp.gene.url}")
 	public void setSolrUrl(String solrUrl) {
 		super.solrUrl = solrUrl;
 	}
-
-	//	@Value("${solr.disease_portal.url}")
-	//	public void setSolrUrl(String solrUrl) {
-	//		super.solrUrl = solrUrl;
-	//	}	
 }
