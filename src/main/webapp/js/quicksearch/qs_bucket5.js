65a5b30438dcb9dd6b8ea61b14506e8c12b5503c
Joel Richardson <joel.richardson@jax.org>
2026-04-09 09:20:31 -0400

    wts2-1557/sprt-283 QS Alleles Tab: remove columns: chr, location, strand

--- src/main/webapp/js/quicksearch/qs_bucket5.js
+++ src/main/webapp/js/quicksearch/qs_bucket5.js
@@ -15,7 +15,7 @@ function b5Show(data) {
 	var toShow = b5PageSize;
 	if (data.rows.length > 0) {
 		tbl = '<TABLE ID="b5Table">';
-		tbl = tbl + '<TR><TH>Score' + qsScoreHelp() + '</TH><TH>Type</TH><TH>Symbol</TH><TH>Name</TH><TH>Chr</TH><TH>Location (Genome Build)</TH><TH>Str</TH><TH>Best Match</TH></TR>';
+		tbl = tbl + '<TR><TH>Score' + qsScoreHelp() + '</TH><TH>Type</TH><TH>Symbol</TH><TH>Name</TH><TH>Best Match</TH></TR>';
 
 		toShow = Math.min(100, data.rows.length);
 		for (var i = 0; i < toShow; i++) {
@@ -34,25 +34,6 @@ function b5Show(data) {
 			}
 			tbl = tbl + '<TD class="nameCol">' + name + '</TD>';
 
-			if (item.chromosome === null) {
-				tbl = tbl + '<TD class="small">&nbsp;</TD>';
-			} else {
-				tbl = tbl + '<TD class="small">' + item.chromosome + '</TD>';
-			}
-
-			if (item.location === null) {
-				tbl = tbl + '<TD>&nbsp;</TD>';
-			} else if (item.location.indexOf('-') < 0){
-				tbl = tbl + '<TD class="nowrap small">' + item.location + '</TD>';
-			} else {
-				tbl = tbl + '<TD class="nowrap small">' + item.location + ' (' + genomeBuild + ')</TD>';
-			}
-
-			if (item.strand === null) {
-				tbl = tbl + '<TD>&nbsp;</TD>';
-			} else {
-				tbl = tbl + '<TD class="small">' + item.strand + '</TD>';
-			}
 			if (item.bestMatchType === null) {
 				tbl = tbl + '<TD>&nbsp;</TD></TR>';
 			} else {
